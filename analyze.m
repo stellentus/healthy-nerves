2d8890f85b58931ab856b8d079f662c8fcd0d579
James Bell <james@bellstone.ca>
2017-09-22 13:30:30 -0600

    Create function for biplot from single nerve
    * Also delete unused code

--- analyze.m
+++ analyze.m
@@ -3,29 +3,38 @@ function [measures, participants, values, shortNames] = analyze()
 	[measures, allParticipants, cData, mData, uniqueMeasures] = importExcel('data/CPrepeatedmeasures.xlsx', 'data/MedianRepeatedmeasures.xlsx');
 
 	% Handle missing data by deleting NaN rows
-	% Create 2 versions of the data:
-	%    1. cData and mData with their individual NaN rows removed
-	%    2. cDataMatched and mDataMatched with NaN removed from both
 	indices = rowsWithNaN(allParticipants, cData, mData);
 	[participants, cDataMatched, mDataMatched] = deleteRows(indices, allParticipants, cData, mData);
-
-	indices = rowsWithNaN(allParticipants, cData);
-	[cParticipants, cData] = deleteRows(indices, allParticipants, cData);
-
-	indices = rowsWithNaN(allParticipants, mData);
-	[mParticipants, mData] = deleteRows(indices, allParticipants, mData);
-	clear indices allParticipants;
 
 	% Print all measures correlated between the datasets.
 	correlations(measures, cDataMatched, mDataMatched);
 
-	% Delete all of the variables we created, but don't actually care to use
-	clear cParticipants, cData; % We don't actually look at CP independently
-	clear mParticipants, mData; % We don't actually look at median independently
-
 	% Combine the arm and leg data into one dataset
 	[measures, values, shortNames] = combineDatasets(measures, cDataMatched, 'CP', mDataMatched, 'Median', uniqueMeasures);
 	clear cDataMatched, mDataMatched;
+end
+
+function biplotCDataWithoutNaN(measures, participants, cData)
+	biplotIndividualWithoutNaN(measures, participants, cData);
+end
+
+function biplotMDataWithoutNaN(measures, participants, mData)
+	biplotIndividualWithoutNaN(measures, participants, mData);
+end
+
+function biplotIndividualWithoutNaN(measures, participants, data)
+	% Handle missing data by deleting NaN rows
+	indices = rowsWithNaN(participants, data);
+	[participants, data] = deleteRows(indices, participants, data);
+	clear indices;
+
+	shortNames = shortenNames(measures);
+
+	[coeff,score] = pca(values, 'VariableWeights', 'variance');
+	biplot(coeff(:,1:3), 'scores', score(:,1:3), 'varlabels', cellstr(shortNames));
+
+	% Print the short and long names of measures.
+	disp([measures' shortNames']);
 end
 
 function biplotWithoutNaN(measures, allParticpants, cData, mData, uniqueMeasures)
