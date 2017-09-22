b0c2f11a1b4d336edbd8cbb685937176188feee2
James Bell <james@bellstone.ca>
2017-09-22 11:27:32 -0600

    Add a command to print biplot without NaN

--- analyze.m
+++ analyze.m
@@ -28,6 +28,25 @@ function [measures, participants, values, shortNames] = analyze()
 	clear cDataMatched, mDataMatched;
 end
 
+function biplotWithoutNaN(measures, allParticpants, cData, mData, uniqueMeasures)
+	% Handle missing data by deleting NaN rows
+	indices = rowsWithNaN(allParticipants, cData, mData);
+	[participants, cDataMatched, mDataMatched] = deleteRows(indices, allParticipants, cData, mData);
+
+	% Print all measures correlated between the datasets.
+	correlations(measures, cDataMatched, mDataMatched);
+
+	% Combine the arm and leg data into one dataset
+	[measures, values, shortNames] = combineDatasets(measures, cDataMatched, 'CP', mDataMatched, 'Median', uniqueMeasures);
+	clear cDataMatched, mDataMatched;
+
+	[coeff,score] = pca(values, 'VariableWeights', 'variance');
+	biplot(coeff(:,1:3), 'scores', score(:,1:3), 'varlabels', cellstr(shortNames));
+
+	% Print the short and long names of measures.
+	disp([measures' shortNames(1:35)']);
+end
+
 function biplotWithALS(measures, cData, mData, uniqueMeasures)
 	[~, values, shortNames] = combineDatasets(measures, cData, 'CP', mData, 'Median', uniqueMeasures);
 
