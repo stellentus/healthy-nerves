f4713c87357321b3a1f3f01fff5593d5fe9fb58a
James Bell <james@bellstone.ca>
2017-09-26 15:16:47 -0600

    Only load the required data

--- analyze.m
+++ analyze.m
@@ -1,24 +1,27 @@
 % analyze runs an analysis on the data
 function [values, participants, measures, shortNames] = analyze(analysisType)
-	[participants, measures, cData, mData, uniqueMeasures] = importExcel('data/CPrepeatedmeasures.xlsx', 'data/MedianRepeatedmeasures.xlsx');
-
 	if nargin == 0
 		analysisType = 'data';
 	end
 
 	switch analysisType
 		case 'data'
+			[participants, measures, cData, mData, uniqueMeasures] = importExcel('data/CPrepeatedmeasures.xlsx', 'data/MedianRepeatedmeasures.xlsx');
 			[values, participants, measures, shortNames] = deleteNaN(participants, measures, cData, mData, uniqueMeasures);
 		case 'delete'
+			[participants, measures, cData, mData, uniqueMeasures] = importExcel('data/CPrepeatedmeasures.xlsx', 'data/MedianRepeatedmeasures.xlsx');
 			[values, participants, measures, shortNames] = deleteNaN(participants, measures, cData, mData, uniqueMeasures);
 			bp(values, measures, shortNames)
 		case 'Cdelete'
+			[participants, measures, cData] = importExcel('data/CPrepeatedmeasures.xlsx');
 			[values, participants, measures, shortNames] = deleteNaN(participants, measures, cData);
 			bp(values, measures, shortNames);
 		case 'Mdelete'
+			[participants, measures, mData] = importExcel('data/MedianRepeatedmeasures.xlsx');
 			[values, participants, measures, shortNames] = deleteNaN(participants, measures, mData);
 			bp(values, measures, shortNames);
 		case 'ALS'
+			[participants, measures, cData, mData, uniqueMeasures] = importExcel('data/CPrepeatedmeasures.xlsx', 'data/MedianRepeatedmeasures.xlsx');
 			[values, measures, shortNames] = combineDatasets(measures, cData, 'CP', mData, 'Median', uniqueMeasures);
 			bp(values, measures, shortNames, alg, 'als')
         otherwise
