728306ad7c13cbd32e798b082874dc9c074944d7
James Bell <james@bellstone.ca>
2017-09-26 15:14:59 -0600

    Move deleteNaN into a function

--- analyze.m
+++ analyze.m
@@ -8,15 +8,15 @@ function [values, participants, measures, shortNames] = analyze(analysisType)
 
 	switch analysisType
 		case 'data'
-			[values, participants, measures, shortNames] = dataWithoutNaN(participants, measures, cData, mData, uniqueMeasures);
+			[values, participants, measures, shortNames] = deleteNaN(participants, measures, cData, mData, uniqueMeasures);
 		case 'delete'
-			[values, participants, measures, shortNames] = dataWithoutNaN(participants, measures, cData, mData, uniqueMeasures);
+			[values, participants, measures, shortNames] = deleteNaN(participants, measures, cData, mData, uniqueMeasures);
 			bp(values, measures, shortNames)
 		case 'Cdelete'
-			[values, participants, measures, shortNames] = dataWithoutNaN(participants, measures, cData);
+			[values, participants, measures, shortNames] = deleteNaN(participants, measures, cData);
 			bp(values, measures, shortNames);
 		case 'Mdelete'
-			[values, participants, measures, shortNames] = dataWithoutNaN(participants, measures, mData);
+			[values, participants, measures, shortNames] = deleteNaN(participants, measures, mData);
 			bp(values, measures, shortNames);
 		case 'ALS'
 			[values, measures, shortNames] = combineDatasets(measures, cData, 'CP', mData, 'Median', uniqueMeasures);
@@ -24,36 +24,4 @@ function [values, participants, measures, shortNames] = analyze(analysisType)
         otherwise
 			disp('ERROR: anaysis type must be one of ''data'', ''delete'', ''Cdelete'', ''Mdelete'', or ''ALS''');
 	end
-end
-
-function [values, participants, measures, shortNames] = dataWithoutNaN(participants, measures, data1, data2, uniqueMeasures)
-	if nargin == 3
-		% Handle missing data by deleting NaN rows
-		indices = rowsWithNaN(participants, data1);
-		[participants, data1] = deleteRows(indices, participants, data1);
-		clear indices;
-
-		shortNames = shortenNames(measures);
-		values = data1;
-	else
-		% Handle missing data by deleting NaN rows
-		indices = rowsWithNaN(participants, data1, data2);
-		[participants, data1Matched, data2Matched] = deleteRows(indices, participants, data1, data2);
-
-		% Print all measures correlated between the datasets.
-		correlations(measures, data1Matched, data2Matched);
-
-		% Combine the arm and leg data into one dataset
-		[values, measures, shortNames] = combineDatasets(measures, data1Matched, 'CP', data2Matched, 'Median', uniqueMeasures);
-	end
-end
-
-function [participants, data1, data2] = deleteRows(indices, participants, data1, data2)
-	data1 = data1(indices, :);
-	if nargin == 4
-		data2 = data2(indices, :);
-	else
-		data2 = [];
-	end
-	participants = participants(indices);
 end
