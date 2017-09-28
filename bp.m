ba248a761611997bca1cda29581c68dcaf2c36ae
James Bell <james@bellstone.ca>
2017-09-28 14:53:24 -0600

    Store the plot dimensions as a variable

--- bp.m
+++ bp.m
@@ -3,6 +3,9 @@ function bp(values, measures, shortNames, alg)
 	if nargin < 4
 		alg = 'svd';
 	end
+
+	plotDim = 1:3;
+
 	[coeff, score] = pca(values, 'VariableWeights', 'variance', 'algorithm', alg);
-	biplot(coeff(:,1:3), 'scores', score(:,1:3), 'varlabels', cellstr(shortNames));
+	biplot(coeff(:, plotDim), 'scores', score(:, plotDim), 'varlabels', cellstr(shortNames));
 end
