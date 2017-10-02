b363d62f8a08b4db669647f56054d6f671b703e8
James Bell <james@bellstone.ca>
2017-10-02 14:28:22 -0600

    Split loadData into a function

--- analyze.m
+++ analyze.m
@@ -52,31 +52,3 @@ function [values, participants, measures] = analyze(dataType, deleteNaN, plotTyp
 	    end
 	end
 end
-
-% loadData loads the CP data, median data, or both
-function [values, participants, measures] = loadData(dataType, shouldDeleteNaN)
-	switch dataType
-		case 'all'
-			[participants, measures, cData, mData, uniqueMeasures] = importExcel('data/CPrepeatedmeasures.xlsx', 'data/MedianRepeatedmeasures.xlsx');
-			if shouldDeleteNaN
-				[values, participants, measures] = deleteNaN(participants, measures, cData, mData, uniqueMeasures);
-			else
-				[values, measures] = combineDatasets(measures, cData, 'CP', mData, 'Median', uniqueMeasures);
-			end
-
-		case 'cp'
-			[participants, measures, values] = importExcel('data/CPrepeatedmeasures.xlsx');
-			if shouldDeleteNaN
-				[values, participants, measures] = deleteNaN(participants, measures, values);
-			end
-
-		case 'median'
-			[participants, measures, values] = importExcel('data/MedianRepeatedmeasures.xlsx');
-			if shouldDeleteNaN
-				[values, participants, measures] = deleteNaN(participants, measures, values);
-			end
-
-		otherwise
-			disp('ERROR: data type must be one of ''all'', ''cp'', or ''median''.');
-	end
-end
