743a834edf5cbc1a14600fc0d413a5d6785a9d51
James Bell <james@bellstone.ca>
2017-10-11 11:39:56 -0600

    Print the values of the strongest weights

--- poster/primaryWeights.m
+++ poster/primaryWeights.m
@@ -10,6 +10,9 @@ function primaryWeights(name, values, measures)
 	% Sort the coefficents by the longest
 	weight = sqrt(dot(coeff, coeff, 2));
 	[~, corder] = sort(weight, 'descend'); % Order the indices to display
-	mdisp = measures(corder(1:measuresToDisplay)); % Get the measures to display, in order
+	cdisp = corder(1:measuresToDisplay); % Get the indices to display, in order
+	mdisp = measures(cdisp); % Get the measures to display, in order
+
 	disp(sprintf('The %d strongest weights for %s are "', measuresToDisplay, name) + strjoin(mdisp, ', ') + '"');
+	disp(sprintf('Those weights are %s', sprintf('%+.2f ', coeff(cdisp, 1))));
 end
