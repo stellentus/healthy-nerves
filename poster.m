7c42495a22a5e2d6ad4bcb57289f1a02d918242d
James Bell <james@bellstone.ca>
2017-10-11 10:34:01 -0600

    Add missing import

--- poster.m
+++ poster.m
@@ -1,7 +1,11 @@
 % poster prints and displays everything for the poster
 function poster(dataType)
 	addpath poster;
+	addpath import;
+
 	count();
 	graphVariance();
+
+	rmpath import;
 	rmpath poster;
 end
