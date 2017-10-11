d88aed11433ff735a64b3a8ecb77cd4f52111234
James Bell <james@bellstone.ca>
2017-10-11 11:08:06 -0600

    Simplify count code

--- poster.m
+++ poster.m
@@ -18,7 +18,8 @@ function poster(dataType)
 
 	addpath poster;
 
-	count(name1, participants1, measures1, name2, participants2, measures2);
+	count(name1, participants1, measures1);
+	count(name2, participants2, measures2);
 	graphVariance(name1, values1, name2, values2);
 
 	rmpath poster;
--- poster/count.m
+++ poster/count.m
@@ -1,10 +1,5 @@
 % count prints the number of remaining participants.
-function count(name1, participants1, measures1, name2, participants2, measures2)
-	countOne(name1, participants1, measures1);
-	countOne(name2, participants2, measures2);
-end
-
-function countOne(displayName, participants, measures)
+function count(displayName, participants, measures)
 	disp(sprintf('%s has %d participants', displayName, length(participants)));
 	disp(sprintf('%s uses %d measures', displayName, length(measures)));
 end
