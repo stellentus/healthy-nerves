d7cd1ee600b41d088f47a1be55c846343a40cbeb
James Bell <james@bellstone.ca>
2017-10-16 16:13:48 -0600

    Update deleteNaN to not use extra rowsWithNaN argument

--- import/deleteNaN.m
+++ import/deleteNaN.m
@@ -3,14 +3,17 @@ function [values, participants, measures] = deleteNaN(participants, measures, da
 	if nargin == 3
 		% Handle missing data by deleting NaN rows
 		indices = rowsWithNaN(participants, data1);
-		[participants, data1] = deleteRows(indices, participants, data1);
+		data1 = data1(indices, :);
+		participants = participants(indices);
 		clear indices;
 
 		values = data1;
 	else
 		% Handle missing data by deleting NaN rows
-		indices = rowsWithNaN(participants, data1, data2);
-		[participants, data1Matched, data2Matched] = deleteRows(indices, participants, data1, data2);
+		indices = rowsWithNaN(participants, data1) & rowsWithNaN(participants, data2);
+		participants = participants(indices);
+		data1Matched = data1(indices, :);
+		data2Matched = data2(indices, :);
 
 		% Print all measures correlated between the datasets.
 		correlations(measures, data1Matched, data2Matched);
@@ -18,14 +21,4 @@ function [values, participants, measures] = deleteNaN(participants, measures, da
 		% Combine the arm and leg data into one dataset
 		[values, measures] = combineDatasets(measures, data1Matched, 'Leg', data2Matched, 'Arm', uniqueMeasures);
 	end
-end
-
-function [participants, data1, data2] = deleteRows(indices, participants, data1, data2)
-	data1 = data1(indices, :);
-	if nargin == 4
-		data2 = data2(indices, :);
-	else
-		data2 = [];
-	end
-	participants = participants(indices);
 end
