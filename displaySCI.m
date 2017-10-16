f269812e5307545e323951722ed0f34e0f24c5fc
James Bell <james@bellstone.ca>
2017-10-16 09:12:11 -0600

    Move displaySCI into top level

--- displaySCI.m
+++ displaySCI.m
@@ -6,12 +6,18 @@ if evalin( 'base', 'exist(''nerve'',''var'') == 1' ) && strcmp(nerve, 'leg')
 	files = struct('ref', 'leg', 'proj', 'arm', 'sci', 'legSCI');
 end
 
+addpath import;
+
 [valuesRef, participantsRef] = loadData(files.ref, true);
 [valuesProj, participantsProj] = loadData(files.proj, true);
 
+addpath analyze;
 coefforthRef = lineCross(valuesRef, valuesProj, participantsRef, participantsProj);
+rmpath analyze;
 
 [valuesSCI] = loadData(files.sci, true);
 scoreSCI = zscore(valuesSCI)*coefforthRef;
 
 plot(scoreSCI(:,1:2), 'xk');
+
+rmpath import;
