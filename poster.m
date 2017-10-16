30db5a85bc169388191c96b6df00be825622fd5c
James Bell <james@bellstone.ca>
2017-10-16 09:37:06 -0600

    Change SCI clustering to more obvious component

--- poster.m
+++ poster.m
@@ -27,7 +27,7 @@ function poster(dataType)
 	graphVariance(nameLeg, valuesLeg, nameArm, valuesArm);
 	ladderPlot(nameLeg, valuesLeg, participantsLeg, nameArm, valuesArm, participantsArm);
 	sciClustering(nameLeg, valuesLeg, participantsLeg, nameArm, valuesArm, participantsArm, valuesLegSCI, 1);
-	sciClustering(nameLeg, valuesLeg, participantsLeg, nameArm, valuesArm, participantsArm, valuesLegSCI, 21);
+	sciClustering(nameLeg, valuesLeg, participantsLeg, nameArm, valuesArm, participantsArm, valuesLegSCI, 28);
 
 	rmpath poster;
 end
