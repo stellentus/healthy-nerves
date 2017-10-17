d9ba8e41f44761052ee77e47404d1d551d2bb749
James Bell <james@bellstone.ca>
2017-10-17 08:44:53 -0600

    Remove unused variable

--- import/rowsWithNaN.m
+++ import/rowsWithNaN.m
@@ -4,12 +4,10 @@ function [indices] = rowsWithNaN(participant, data1)
 	cNan = sum(isnan(data1), 2);
 
 	indices = true(1, length(participant));
-	deletions = [];
 	for i = 1:length(participant)
 		% If the dataset contains NaN for this index, flag it.
 		if cNan(i) > 0
 			indices(i) = false;
-			deletions = [deletions; participant(i)];
 		end
 	end
 end
