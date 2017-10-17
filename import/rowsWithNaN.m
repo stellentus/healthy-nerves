877de1cbd28cbb9a38ca826d4d4c5cff8264dee9
James Bell <james@bellstone.ca>
2017-10-17 08:46:08 -0600

    Simplify function

--- import/rowsWithNaN.m
+++ import/rowsWithNaN.m
@@ -1,13 +1,5 @@
 % rowsWithNaN returns any row with NaN.
 function [indices] = rowsWithNaN(participant, data1)
 	% Calculate which rows contain NaN for each type
-	cNan = sum(isnan(data1), 2);
-
-	indices = true(1, length(participant));
-	for i = 1:length(participant)
-		% If the dataset contains NaN for this index, flag it.
-		if cNan(i) > 0
-			indices(i) = false;
-		end
-	end
+	indices = sum(isnan(data1), 2) == 0;
 end
