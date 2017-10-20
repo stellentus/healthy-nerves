f67b6368bf44ab79a2a151e0d7b8b39fb5ebead8
James Bell <james@bellstone.ca>
2017-10-20 11:45:40 -0600

    Display additional correlations

--- poster/correlations.m
+++ poster/correlations.m
@@ -4,6 +4,15 @@ function correlations(values1, values2, participants1, participants2)
 	values2 = values2(ind2, :);
 
 	printCorr(values1, values2, 'original');
+
+	[~, score1] = pca(values1, 'VariableWeights', 'variance');
+	[~, score2] = pca(values2, 'VariableWeights', 'variance');
+	printCorr(score1, score2, 'pca');
+
+	[coefforth, meanorth] = transform(values1);
+	score1 = values1*coefforth-meanorth;
+	score2 = values2*coefforth-meanorth;
+	printCorr(score1, score2, 'transformed');
 end
 
 % correlations prints the list of measurements that are correlated between two measures
