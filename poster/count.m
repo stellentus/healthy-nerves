9ecc2281c90795109c9c5f33c2ca6097ac2b06e8
James Bell <james@bellstone.ca>
2017-10-20 10:42:23 -0600

    Improve count output

--- poster/count.m
+++ poster/count.m
@@ -1,5 +1,4 @@
 % count prints the number of remaining participants.
 function count(displayName, participants, measures)
-	disp(sprintf('%s has %d participants', displayName, length(participants)));
-	disp(sprintf('%s uses %d measures', displayName, length(measures)));
+	disp(sprintf('%s has %d participants with %d measures', displayName, length(participants), length(measures)));
 end
