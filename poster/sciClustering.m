9c649d304c4cc771eef0e1bd725c7d609d0b7e75
James Bell <james@bellstone.ca>
2017-10-25 10:42:15 -0600

    Add optional markersize to scliClustering

--- poster/sciClustering.m
+++ poster/sciClustering.m
@@ -1,5 +1,5 @@
 % sciClustering attempts to print clustered SCI results
-function sciClustering(name1, values1, participants1, valuesSCI, participantsSCI, component)
+function sciClustering(name1, values1, participants1, valuesSCI, participantsSCI, component, markersize)
 	[coefforth, meanorth] = transform(values1);
 
 	score1 = values1*coefforth-meanorth;
@@ -12,13 +12,19 @@ function sciClustering(name1, values1, participants1, valuesSCI, participantsSCI
 
 	for i = 1:size(score1, 1)
 		color = colors(mod(i, colorMod)+1, :);
-		plot(score1(i,component), score1(i,component+1), '.', 'Color', color);
+		h = plot(score1(i,component), score1(i,component+1), '.', 'Color', color);
+		if nargin == 7
+			set(h, 'markersize', markersize);
+		end
 	end
 
 	scoreSCI = valuesSCI*coefforth-meanorth;
 	for i = 1:size(scoreSCI, 1)
 		h = plot(scoreSCI(i,component), scoreSCI(i,component+1), '*k');
 		label(h, participantsSCI(i));
+		if nargin == 7
+			set(h, 'markersize', markersize);
+		end
 	end
 
 	xlabel(sprintf('Component %d', component));
