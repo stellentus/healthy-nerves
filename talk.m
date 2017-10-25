12685eb3a182f421fe5cdf373ec9e9b1526d5eb0
James Bell <james@bellstone.ca>
2017-10-25 17:11:21 -0600

    Improve image for talk

--- talk.m
+++ talk.m
@@ -3,19 +3,35 @@ function talk(dataType)
 	shouldDeleteNaN = true;
 
 	addpath import;
-
-	nameLeg = 'Leg';
-	[valuesLeg, participantsLeg, measuresLeg, ~, age, sex] = mefimport(pathFor('leg'), shouldDeleteNaN);
-	[~, ~, ~, ~, ageTotal, sexTotal] = mefimport(pathFor('leg'), false);
-
-	% Load SCI data
-	[valuesLegSCI, participantsSCI] = mefimport(pathFor('legSCI'), shouldDeleteNaN);
-
+	valuesLeg = mefimport(pathFor('leg'), shouldDeleteNaN);
+	valuesLegSCI = mefimport(pathFor('legSCI'), shouldDeleteNaN);
 	rmpath import;
 
 	addpath poster;
-
-	sciClustering(nameLeg, valuesLeg, participantsLeg, valuesLegSCI, participantsSCI, 1, 20);
-
+	[coefforth, meanorth] = transform(valuesLeg);
 	rmpath poster;
+
+	component = 1;
+
+	score1 = valuesLeg*coefforth-meanorth;
+
+	hold on;
+
+	colors = get(gca,'colororder');
+	colorMod = length(colors);
+
+	for i = 1:size(score1, 1)
+		color = colors(mod(i, colorMod)+1, :);
+		plot(score1(i,component), score1(i,component+1), '.', 'Color', [.745 .706 .549], 'MarkerSize', 20);
+	end
+
+	scoreSCI = valuesLegSCI*coefforth-meanorth;
+	for i = 1:size(scoreSCI, 1)
+		plot(scoreSCI(i,component), scoreSCI(i,component+1), 'p', 'Color', [0 .384 .192], 'MarkerSize', 20, 'MarkerFaceColor', 'black');
+		plot(scoreSCI(i,component), scoreSCI(i,component+1), 'o', 'Color', [0 .384 .192], 'MarkerSize', 20);
+	end
+
+	xlabel(sprintf('Component %d', component), 'Color', [0 .384 .192]);
+	ylabel(sprintf('Component %d', component+1), 'Color', [0 .384 .192]);
+	title('Healthy vs SCI (CP Nerve)', 'FontSize', 24, 'Color', [0 .384 .192]);
 end
