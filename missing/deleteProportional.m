4b8a07b627434056df79a52c4e08a8c29dcf2358
James Bell <james@stellentus.com>
2018-03-05 15:24:19 -0700

    Ensure train/test split is always 140:25

--- missing/deleteProportional.m
+++ missing/deleteProportional.m
@@ -1,8 +1,5 @@
 % deleteProportional deletes proportional amounts of missing data
-function [X_missing, X_full, X] = deleteProportional(X)
-	X = X(randperm(size(X,1)),:); % Shuffle
-
-	% Sort X into train and test sets
+function [X_missing, X_full, mask] = deleteProportional(X)
 	TOTAL_COUNT = size(X, 1);
 
 	% Get indices of values to delete
@@ -11,17 +8,17 @@ function [X_missing, X_full, X] = deleteProportional(X)
 	delRef25 = delRef2(randi(length(delRef2), round(.02/.096*TOTAL_COUNT), 1));
 	delTEh = randi(TOTAL_COUNT, round(.015*TOTAL_COUNT), 1);
 
-	% Delete values
-	X(delHyper, 25) = 0;
-	X(delRef2, 29) = 0;
-	X(delRef25, 26) = 0;
-	X(delTEh, 21) = 0;
+	% Create mask
+	mask = ones(size(X));
+	mask(delHyper, 25) = 0;
+	mask(delRef2, 29) = 0;
+	mask(delRef25, 26) = 0;
+	mask(delTEh, 21) = 0;
 
 	missingIndices = union(delHyper, union(delRef2, delTEh));
 	fullIndices = setxor(missingIndices, [1:TOTAL_COUNT]);
 
-	X_missing = X(missingIndices);
-	X_full = X(fullIndices);
-
-	X = X([fullIndices; missingIndices]);
+	Xdel = X .* mask;
+	X_missing = Xdel(missingIndices, :);
+	X_full = X(fullIndices, :);
 end
--- missmiss.m
+++ missmiss.m
@@ -1,5 +1,5 @@
 % missmiss loads missing data and does stuff with it
-function [X_missing, X_full, X] = missmiss()
+function [X_missing, X_full, X, mask] = missmiss()
 	% Load data
 	addpath import;
 	armValues = mefimport(pathFor('arm'), true);
@@ -11,6 +11,13 @@ function [X_missing, X_full, X] = missmiss()
 	clear armValues legValues;
 
 	addpath missing
-	[X_missing, X_full, X] = deleteProportional(X);
+	numMissing = 0;
+	count = 0;
+	while numMissing ~= 25
+		[X_missing, X_full, mask] = deleteProportional(X);
+		numMissing = size(X_missing, 1);
+		count = count + 1;
+	end
+	fprintf('The process took %d iterations.\n', count)
 	rmpath missing
 end
