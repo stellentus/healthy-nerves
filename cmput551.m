9099b77789a8d61aa4ab19d6a1cdef55b59a9845
James Bell <james@stellentus.com>
2018-03-05 14:30:57 -0700

    Parameterize deletion

--- cmput551.m
+++ cmput551.m
@@ -12,15 +12,18 @@ function cmput551()
 	X = X(randperm(size(X,1)),:); % Shuffle
 
 	% Sort X into train and test sets
-	Xtrain = X(1:125, :);
-	Xtest = X(126:165, :);
+	TOTAL_COUNT = 165;
+	TEST_COUNT = 40;
+	TRAIN_COUNT = TOTAL_COUNT - TEST_COUNT;
+	Xtrain = X(1:TRAIN_COUNT, :);
+	Xtest = X(TRAIN_COUNT+1:165, :);
 	Xtest_orig = Xtest;
 
 	% Get indices of values to delete
-	delHyper = randi(40, 17, 1);
-	delRef2 = randi(40, 30, 1);
+	delHyper = randi(TEST_COUNT, 17, 1);
+	delRef2 = randi(TEST_COUNT, 30, 1);
 	delRef25 = delRef2(randi(length(delRef2), 6, 1));
-	delTEh = randi(40, 5, 1);
+	delTEh = randi(TEST_COUNT, 5, 1);
 
 	% Delete values
 	Xtest(delHyper, 25) = 0;
