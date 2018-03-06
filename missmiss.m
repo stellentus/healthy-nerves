69b20909fffd03dece563bf39b4564c1829aa9d5
James Bell <james@stellentus.com>
2018-03-06 15:44:13 -0700

    Allow multiple iterations

--- missmiss.m
+++ missmiss.m
@@ -1,5 +1,9 @@
 % missmiss loads missing data and does stuff with it
-function [missingX, completeX, mask, originalMissingX, missingMask] = missmiss()
+function [missingX, completeX, mask, originalMissingX, missingMask] = missmiss(iters)
+	if nargin == 0
+		iters = 1;
+	end
+
 	X = loadMEF();
 
 	addpath missing
@@ -14,14 +18,22 @@ function [missingX, completeX, mask, originalMissingX, missingMask] = missmiss()
 		@fillMI,
 	};
 
-	[missingX, completeX, mask, originalMissingX, missingMask] = deleteProportional(X);
+	verrs = [[]];
+	cerrs = [[]];
 
-	[verr, cerr] = testFuncs(funcs, missingX, completeX, mask, originalMissingX, missingMask, originalCov);
+	for i = 1:iters
+		fprintf('Running iteration %d of %d...\n', i, iters);
+		[missingX, completeX, mask, originalMissingX, missingMask] = deleteProportional(X);
+		[verr, cerr] = testFuncs(funcs, missingX, completeX, mask, originalMissingX, missingMask, originalCov);
+		verrs = [verrs; verr];
+		cerrs = [cerrs; cerr];
+	end
+	fprintf('\n');
 
-	fprintf(' Algorithm | Value Error | Covariance Error\n');
-	fprintf('-----------+-------------+------------------\n');
+	fprintf(' Algorithm | Value Error (std dev) | Covariance Error (std dev)\n');
+	fprintf('-----------+-----------------------+----------------------------\n');
 	for i = 1:length(funcs)
-		fprintf('%10s | %9s   | %9s\n', func2str(funcs{i}), num2str(verr(i), '%.2f'), num2str(cerr(i), '%.2f'));
+		fprintf('%10s | %11s (%5s)   | %11s (%5s)\n', func2str(funcs{i}), num2str(mean(verr(:, i)), '%.2f'), num2str(std(verr(:, i)), '%.2f'), num2str(mean(cerr(:, i)), '%.2f'), num2str(std(cerr(:, i)), '%.2f'));
 	end
 
 	rmpath missing
