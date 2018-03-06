a4828a61350ffc7865c58cfb28826540686b1953
James Bell <james@stellentus.com>
2018-03-06 15:14:58 -0700

    Add code to import TE data

--- missmiss.m
+++ missmiss.m
@@ -1,14 +1,23 @@
 % missmiss loads missing data and does stuff with it
-function [missingX, completeX, mask, originalMissingX, missingMask] = missmiss()
+function [missingX, completeX, mask, originalMissingX, missingMask, delayTEd40, TEd40] = missmiss()
 	% Load data
 	addpath import;
-	armValues = mefimport(pathFor('arm'), true);
-	legValues = mefimport(pathFor('leg'), true);
+	[armValues, armParticipants] = mefimport(pathFor('arm'), true);
+	[legValues, legParticipants] = mefimport(pathFor('leg'), true);
+
+	[armDelayTEd40, armDelayTEh40, ~, ~, armTEd40, armTEh40, ~, ~] = mefTEimport(pathFor('arm'), armParticipants);
+	[legDelayTEd40, legDelayTEh40, ~, ~, legTEd40, legTEh40, ~, ~] = mefTEimport(pathFor('leg'), legParticipants);
+	clear armParticipants legParticipants;
 	rmpath import;
 
 	% Store all samples as X
 	X = [armValues; legValues];
 	clear armValues legValues;
+
+	delayTEd40 = armDelayTEd40';
+	clear armDelayTEd40 legDelayTEd40;
+	TEd40 = [armTEd40'; legTEd40'];
+	clear armTEd40 legTEd40;
 
 	addpath missing
 
