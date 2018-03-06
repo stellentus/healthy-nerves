c22c9b3e085926e72610e8e033fdbb7f7ca0ce24
James Bell <james@stellentus.com>
2018-03-06 12:33:33 -0700

    Simplify calculation of covariance error

--- missing/covError.m
+++ missing/covError.m
@@ -1,11 +1,4 @@
 % covError compares cov(X) values
 function [err] = covError(originalCov, filledCov)
-	err = 0;
-	for i = 1:size(originalCov, 1)
-		for j = 1:size(originalCov, 2)
-			difference = originalCov(i, j) - filledCov(i, j);
-			err = err + (difference ^ 2);
-		end
-	end
-	err = err / (size(originalCov, 1) * size(originalCov, 2));
+	err = mean(mean((originalCov - filledCov) .^ 2));
 end
