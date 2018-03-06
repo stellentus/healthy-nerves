fea8cc624e808a06ede671f4983a26ae75c06b48
James Bell <james@stellentus.com>
2018-03-06 12:40:36 -0700

    Simplify calculation of value error

--- missing/valueError.m
+++ missing/valueError.m
@@ -1,21 +1,5 @@
 % valueError compares X values
 function [err] = valueError(missingX, originalMissingX, missingMask, filledX)
-	err = 0;
-	count = 0;
-	for j = 1:size(missingX, 2)
-		% Skip this column if it has no missing values
-		if sum(missingMask(:, j)) == 0
-			continue
-		end
-
-		for i = 1:size(missingX, 1)
-			if isnan(missingX(i, j))
-				difference = originalMissingX(i, j) - filledX(i, j);
-				err = err + (difference ^ 2);
-				count = count + 1;
-			end
-		end
-	end
-
-	err = err / count;
+	mask = ~missingMask;
+	err = sum(sum(((originalMissingX - filledX) .* mask) .^ 2)) / sum(sum(mask));
 end
