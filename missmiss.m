15e01898cf1421fc64dd69db12e39bbc1f0e0d12
James Bell <james@stellentus.com>
2018-04-11 16:05:08 -0600

    Simplify enable/disable of algorithms

--- missmiss.m
+++ missmiss.m
@@ -1,5 +1,5 @@
 % missmiss loads missing data and does stuff with it
-function [X, covr, verrs, cerrs, names] = missmiss(iters)
+function [X, covr, verrs, cerrs, algs] = missmiss(iters)
 	if nargin == 0
 		iters = 1;
 	end
@@ -11,65 +11,44 @@ function [X, covr, verrs, cerrs, names] = missmiss(iters)
 	covr = cov(X);
 
 	% Set up functions to iterate through
-	funcs = {
-		@fillCCA,
-		@fillMean,
-		@fillPCA,
-		@fillRegr,
-		@fillNeural,
-		@fillMultiNeural,
-		@fillAutoencoder,
-		@fillAutoencoder,
-	};
-	names = {
-		'Listwise',
-		'Mean-Fill',
-		'PCA (k=4)',
-		'Regression',
-		'Neural',
-		'MultNeural',
-		'Autoencode',
-		'AutoSGD',
-	};
-	args = {
-		{},
-		{},
-		4,
-		{},
-		struct('adadelta', true),
-		struct('adadelta', true),
-		struct('adadelta', true),
-		struct(),
-	};
+	algs = [];
+	algs = [algs; struct('func', @fillCCA, 'name', 'Listwise', 'args', [])];
+	algs = [algs; struct('func', @fillMean, 'name', 'Mean-Fill', 'args', [])];
+	algs = [algs; struct('func', @fillPCA, 'name', 'PCA (k=4)', 'args', 4)];
+	algs = [algs; struct('func', @fillRegr, 'name', 'Regression', 'args', [])];
+	algs = [algs; struct('func', @fillNeural, 'name', 'Neural', 'args', struct('adadelta', true))];
+	algs = [algs; struct('func', @fillMultiNeural, 'name', 'MultNeural', 'args', struct('adadelta', true))];
+	algs = [algs; struct('func', @fillAutoencoder, 'name', 'Autoencode', 'args', struct('adadelta', true))];
+	algs = [algs; struct('func', @fillAutoencoder, 'name', 'AutoSGD', 'args', struct())];
 
 	% Calculate errors
 	verrs = [[]];
 	cerrs = [[]];
 	for i = 1:iters
 		fprintf('Iter %d of %d...', i, iters);
-		[verr, cerr] = testFuncs(funcs, names, X, covr, args);
+		[verr, cerr] = testFuncs(algs, X, covr);
 		verrs = [verrs; verr];
 		cerrs = [cerrs; cerr];
 	end
 	fprintf('\n');
 
-	save('bin/vars.mat', 'X', 'covr', 'verrs', 'cerrs', 'names');
+	save('bin/vars.mat', 'X', 'covr', 'verrs', 'cerrs', 'algs');
 
 	% Print table of values
 	fprintf(' Algorithm | Value Error (std dev) | Covariance Error (std dev)\n');
 	fprintf('-----------+-----------------------+----------------------------\n');
-	for i = 1:length(funcs)
-		fprintf('%10s | %11s (%5s)   | %11s (%5s)\n', names{i}, num2str(mean(verrs(:, i)), '%.2f'), num2str(std(verrs(:, i)), '%.2f'), num2str(mean(cerrs(:, i)), '%.2f'), num2str(std(cerrs(:, i)), '%.2f'));
+	for i = 1:length(algs)
+		fprintf('%10s | %11s (%5s)   | %11s (%5s)\n', algs(i).name, num2str(mean(verrs(:, i)), '%.2f'), num2str(std(verrs(:, i)), '%.2f'), num2str(mean(cerrs(:, i)), '%.2f'), num2str(std(cerrs(:, i)), '%.2f'));
 	end
 
 	if iters ~= 1
 		% Calculate statistical significance
-		calcStats(verrs, 'Values', names)
-		calcStats(cerrs, 'Covariances', names)
+		calcStats(verrs, 'Values', algs)
+		calcStats(cerrs, 'Covariances', algs)
 	end
 
 	% Plot values
-	plotBoxes(verrs, cerrs, names);
+	plotBoxes(verrs, cerrs, algs);
 
 	rmpath missing
 end
@@ -88,7 +67,7 @@ function [X] = loadMEF()
 	TEd40 = [armTEd40'; legTEd40']; % Currently unused
 end
 
-function [verr, cerr] = testFuncs(funcs, names, X, originalCov, args)
+function [verr, cerr] = testFuncs(algs, X, originalCov)
 	fprintf('Load...');
 	[missingX, completeX, mask, originalMissingX, missingMask] = deleteProportional(X);
 
@@ -98,34 +77,34 @@ function [verr, cerr] = testFuncs(funcs, names, X, originalCov, args)
 	seed = randi(2^32-1); % Generate a new seed randomly, but save it
 
 	fprintf('Run ');
-	for i = 1:length(funcs)
-		fprintf('%s...', names{i});
+	for i = 1:length(algs)
+		fprintf('%s...', algs(i).name);
 		rng(seed); % Seed each algorithm with the exact same random numbers
-		[filledX, covr] = funcs{i}(missingX, completeX, mask, originalMissingX, missingMask, args{i});
+		[filledX, covr] = algs(i).func(missingX, completeX, mask, originalMissingX, missingMask, algs(i).args);
 		verr = [verr valueError(missingX, originalMissingX, missingMask, filledX)];
 		cerr = [cerr covError(originalCov, covr)];
 	end
 	fprintf('\n');
 end
 
-function calcStats(data, name, names)
+function calcStats(data, name, algs)
 	for i = 1:size(data, 2)
 		for j = i+1:size(data, 2)
 			[h, p] = ttest(data(:, i), data(:, j));
 			if h == 1
 				if p < 0.001
-					fprintf('** %10s vs %10s is significant (p=%f)\n', names{i}, names{j}, p);
+					fprintf('** %10s vs %10s is significant (p=%f)\n', algs(i).name, algs(j).name, p);
 				else
-					fprintf('*  %10s vs %10s is significant (p=%.3f)\n', names{i}, names{j}, p);
+					fprintf('*  %10s vs %10s is significant (p=%.3f)\n', algs(i).name, algs(j).name, p);
 				end
 			else
-				fprintf('   %10s vs %10s is not significant (p=%.3f)\n', names{i}, names{j}, p);
+				fprintf('   %10s vs %10s is not significant (p=%.3f)\n', algs(i).name, algs(j).name, p);
 			end
 		end
 	end
 end
 
-function plotBoxes(verrs, cerrs, names)
+function plotBoxes(verrs, cerrs, algs)
 	% Replace NaN with a really big number. This prevents errors in plotting.
 	verrs(isnan(verrs)) = realmax;
 	cerrs(isnan(cerrs)) = realmax;
@@ -135,14 +114,14 @@ function plotBoxes(verrs, cerrs, names)
 	figure('DefaultAxesFontSize', 18);
 
 	subplot(1, 2, 1);
-	CategoricalScatterplot(verrs, names);
+	CategoricalScatterplot(verrs, [char(algs.name)]);
 	ylim([0 200]);
 	title('A) Error in Filled Data');
 	xlabel('Method');
 	ylabel('Error');
 
 	subplot(1, 2, 2);
-	CategoricalScatterplot(cerrs, names);
+	CategoricalScatterplot(cerrs, [char(algs.name)]);
 	ylim([0 200]);
 	title('B) Error in Covariance');
 	xlabel('Method');
