3953b1a9c8f6f9eb05fe6b3545c460b9e0c763bb
James Bell <james@stellentus.com>
2018-04-13 09:01:12 -0600

    Implement initial fancy autoencoder

--- missing/fillFancyAuto.m
+++ missing/fillFancyAuto.m
@@ -26,12 +26,11 @@ function [filledX, covr] = fillFancyAuto(missingX, completeX, mask, originalMiss
 	trainData = [completeX; missingX];
 
 	% Train
-	model = fancyNeural(model);
-	model = fancyNeural(model, trainData(:, completeIndices), trainData(:, missIndices));
+	model = reset(model);
+	model = learn(model, trainData(:, completeIndices), trainData(:, missIndices));
 
 	% Predict
-	model = fancyNeural(model, missingX(:, completeIndices));
-	missY = model.missing;
+	missY = predict(model, missingX(:, completeIndices), missingX(:, missIndices));
 
 	for j_ind = 1:length(missIndices)
 		j = missIndices(j_ind);
@@ -44,24 +43,6 @@ function [filledX, covr] = fillFancyAuto(missingX, completeX, mask, originalMiss
 	end
 
 	covr = calcCov(completeX, filledX);
-end
-
-
-% fancyNeural does a whole bunch of stuff related to a neural network.
-% If no additional arguments are provided, it initializes and resets.
-% If two additional arguments are provided, they are used as Xtrain and missing.
-% If one additional argument is provided, it's used as Xtest.
-function [self] = fancyNeural(self, X, missing)
-	switch nargin
-		case 1
-			self = reset(self);
-		case 3 % Xtrain, missing
-			self = learn(self, X, missing);
-		case 2 % Xtest
-			self.missing = predict(self, X);
-		otherwise
-			disp('Error in number of arguments to Neural Network.')
-	end
 end
 
 function [self] = reset(self)
@@ -77,30 +58,13 @@ function [self] = reset(self)
 	if ~isfield(self.params, 'epochs')
 		self.params.epochs = 200;
 	end
-	if ~isfield(self.params, 'sigmoid')
-		self.params.sigmoid = false;
-	end
 	if ~isfield(self.params, 'rho')
 		self.params.rho = 0.99; % .997 performs slightly better, but .999 is much worse
-	end
-	if ~isfield(self.params, 'useSGD')
-		self.params.useSGD = false;
-	end
-
-	if self.params.sigmoid
-		self.transfer = @sigmoid;
-	else
-		self.transfer = @linearTrans;
-	end
-
-	if self.params.useSGD
-		self.learnAlg = @learnSGD;
-	else
-		self.learnAlg = @learnAdadelta;
 	end
 
 	self.w_input = 0;
 	self.w_output = 0;
+	self.w_missing = 0;
 end
 
 % Returns the output of the current neural network for the given input
@@ -108,104 +72,104 @@ function [a_output, a_hidden] = feedforward(self, inputs)
 	inputs(isnan(inputs)) = 0; % Zero NaN
 
 	% hidden activations
-	a_hidden = self.transfer(self.w_input * inputs');
+	a_hidden = self.w_input * inputs';
 
 	% output activations
-	a_output = self.transfer(self.w_output * a_hidden);
+	a_output = self.w_output * a_hidden;
+end
+
+function [missing, nabla_miss] = frontBackMissing(self, yhat, h, expectedMissing)
+	nabla_miss = zeros(self.size.w_missing);
+	missing = zeros(size(expectedMissing));
+
+	missing(:, 1) = yhat(self.numoutputs, :);
+
+	for i=[1:self.nummissing]
+		% TODO feed in the correct values when possible
+		hiddenPlus = [h; missing(:, 1:i)'];
+		thisMiss = self.w_missing(i, 1:self.params.nh+i) * hiddenPlus;
+		nabla_miss(i, 1:self.params.nh+i) = (thisMiss' - expectedMissing(:, i))' * hiddenPlus';
+		missing(:, i+1) = thisMiss;
+	end
+
+	nabla_miss(isnan(nabla_miss)) = 0;
+
+	assert(sum(size(nabla_miss)==size(self.w_missing)) == ndims(nabla_miss));
 end
 
 % Return a tuple ``(nabla_input, nabla_output)`` representing the gradients for the cost function with respect to self.w_input and self.w_output.
-function [nabla_input, nabla_output] = backprop(self, x, y)
+function [nabla_input, nabla_output, nabla_miss] = backprop(self, x, missing)
 	[yhat, h] = feedforward(self, x);
-	dshare = yhat' - y;
+	dshare = yhat' - x;
 	nabla_output = dshare' * h';
 
-	nabla_input = zeros(self.params.nh, self.numfeatures);
-	sigmoidTerm = ones(1, self.params.nh);
-	if self.params.sigmoid
-		sigmoidTerm = (h .* (1 - h))';
-	end
-
-	nabla_input = (dshare * self.w_output .* sigmoidTerm)' * x;
+	nabla_input = (dshare * self.w_output)' * x;
 
 	% If a value was NaN, don't backpropogate it.
 	nabla_output(isnan(nabla_output)) = 0;
 	nabla_input(isnan(nabla_input)) = 0;
 
-	% assert(size(nabla_input, 1) == size(self.w_input, 1));
-	% assert(size(nabla_input, 2) == size(self.w_input, 2));
-	% assert(size(nabla_output, 1) == size(self.w_output, 1));
-	% assert(size(nabla_output, 2) == size(self.w_output, 2));
+	% assert(sum(size(nabla_input)==size(self.w_input)) == ndims(nabla_input));
+	% assert(sum(size(nabla_output)==size(self.w_output)) == ndims(nabla_output));
+
+	% Next backpropogate missing values
+	[~, nabla_miss] = frontBackMissing(self, yhat, h, missing);
 end
 
 % Learns using the traindata with batch gradient descent.
 function [self] = learn(self, Xtrain, missing)
-	ytrain = [Xtrain missing];
-
 	self.numfeatures = size(Xtrain, 2);
-	self.numoutputs = size(ytrain, 2);
+	self.numoutputs = size(Xtrain+1, 2); % Add the first missing column to output
 	self.numsamples = size(Xtrain, 1);
+	self.nummissing = size(missing, 2)-1; % Minus one because self.numoutputs contains one of them.
 	perturbation = 1;
 
-	self.w_output = normrnd(0.0, perturbation, [self.numoutputs, self.params.nh]);
-	self.w_input = normrnd(0.0, perturbation, [self.params.nh, self.numfeatures]);
+	self.size = struct();
+	self.size.w_output = [self.numoutputs, self.params.nh];
+	self.size.w_input = [self.params.nh, self.numfeatures];
+	self.size.w_missing = [self.nummissing, self.params.nh+self.nummissing];
 
-	% fprintf('\n000: Cost is %f\n', norm(feedforward(self, Xtrain)' - ytrain));
-	self = self.learnAlg(self, Xtrain, ytrain);
+	self.w_output = normrnd(0.0, perturbation, self.size.w_output);
+	self.w_input = normrnd(0.0, perturbation, self.size.w_input);
+	self.w_missing = normrnd(0.0, perturbation, self.size.w_missing);
+
+	self = learnAdadelta(self, Xtrain, missing);
 end
 
 % Use the parameters computed in self.learn to give predictions on new observations.
-function [yhat] = predict(self, Xtest)
-	yhat = feedforward(self, Xtest)';
-	lastCompleteInd = size(Xtest, 2);
-	lastInd = size(yhat, 2);
-	yhat = yhat(:, [lastCompleteInd+1:lastInd]);
+function [missing] = predict(self, Xtest, missing)
+	[yhat, h] = feedforward(self, Xtest);
+	missing = frontBackMissing(self, yhat, h, missing);
 end
 
-function [self] = learnSGD(self, Xtrain, ytrain)
+function [self] = learnAdadelta(self, Xtrain, missing)
 	for i = [1:self.params.epochs]
 		% Shuffle indexes
 		ind = randperm(self.numsamples);
 		n = self.params.epsilon / (i + 1);
 
-		for j_ind = [1:self.numsamples]
-			j = ind(j_ind);
-
-			[nabla_input, nabla_output] = backprop(self, Xtrain(j, :), ytrain(j, :));
-
-			self.w_input = self.w_input - n * nabla_input;
-			self.w_output = self.w_output - n * nabla_output;
-		end
-
-		% fprintf('%03i: Cost is %f\n', i, norm(feedforward(self, Xtrain)' - ytrain));
-	end
-end
-
-function [self] = learnAdadelta(self, Xtrain, ytrain)
-	for i = [1:self.params.epochs]
-		% Shuffle indexes
-		ind = randperm(self.numsamples);
-		n = self.params.epsilon / (i + 1);
-
-		exp_nab_out = zeros(self.numoutputs, self.params.nh);
-		exp_nab_in = zeros(self.params.nh, self.numfeatures);
-		exp_w_out = zeros(self.numoutputs, self.params.nh);
-		exp_w_in = zeros(self.params.nh, self.numfeatures);
+		exp_nab_out = zeros(self.size.w_output);
+		exp_nab_in = zeros(self.size.w_input);
+		exp_nab_miss = zeros(self.size.w_missing);
+		exp_w_out = zeros(self.size.w_output);
+		exp_w_in = zeros(self.size.w_input);
+		exp_w_miss = zeros(self.size.w_missing);
 
 		for j_ind = [1:self.numsamples]
 			j = ind(j_ind);
 
 			% Compute gradient
-			[nabla_input, nabla_output] = backprop(self, Xtrain(j, :), ytrain(j, :));
+			[nabla_input, nabla_output, nabla_miss] = backprop(self, Xtrain(j, :), missing(j, :));
 
 			[exp_nab_in, exp_w_in, delta_w_in] = AdadeltaUpdate(self, exp_nab_in, exp_w_in, nabla_input);
 			self.w_input = self.w_input + delta_w_in;
 
 			[exp_nab_out, exp_w_out, delta_w_out] = AdadeltaUpdate(self, exp_nab_out, exp_w_out, nabla_output);
 			self.w_output = self.w_output + delta_w_out;
-		end
 
-		% fprintf('%03i: Cost is %f\n', i, norm(feedforward(self, Xtrain)' - ytrain));
+			[exp_nab_miss, exp_w_miss, delta_w_miss] = AdadeltaUpdate(self, exp_nab_miss, exp_w_miss, nabla_miss);
+			self.w_missing = self.w_missing + delta_w_miss;
+		end
 	end
 end
 
