939a45581771ed7fde78ed3dd8355960ea1376c4
James Bell <james@stellentus.com>
2018-04-23 16:40:18 -0600

    Improve PCA performance

--- missing/fillPCA.m
+++ missing/fillPCA.m
@@ -8,6 +8,20 @@ function [filledX] = fillPCA(missingX, completeX, mask, originalMissingX, missin
 		args.VariableWeights = ones(1, size(missingX, 2));
 	end
 
+	% The following three arguments provide different ways to initialize the NaN values. It doesn't make sense to set more than one of them.
+	% The all perform about the same, with no significant difference between zeroNaN and randomNaN. meanNaN is barely worse.
+	if isfield(args, 'zeroNaN') && args.zeroNaN
+		missingX(isnan(missingX)) = 0;
+	end
+	if isfield(args, 'meanNaN') && args.meanNaN
+		meanX = ones(size(missingX, 2), 1)*nanmean([missingX; completeX]);
+		missingX(isnan(missingX)) = meanX(isnan(missingX));
+	end
+	if isfield(args, 'randomNaN') && args.randomNaN
+		randX = rand(size(missingX));
+		missingX(isnan(missingX)) = randX(isnan(missingX));
+	end
+
 	X = [missingX; completeX];
 	[coeff, score, ~, ~, ~, mu] = pca(X, 'VariableWeights', args.VariableWeights, 'algorithm', 'als');
 	coeff = coeff';
