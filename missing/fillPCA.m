7fdf9547f4ed3f3dee10d006d5219cae878623cc
James Bell <james@stellentus.com>
2018-04-23 12:49:18 -0600

    Add a comment

--- missing/fillPCA.m
+++ missing/fillPCA.m
@@ -1,4 +1,5 @@
 % fillPCA uses PCA with ALS to fill the values.
+%   filledX is the best-guess filling of the missing values (with known values used when possible).
 function [filledX, covr] = fillPCA(missingX, completeX, mask, originalMissingX, missingMask, args)
 	if ~isfield(args, 'k')
 		args.k = 4;
