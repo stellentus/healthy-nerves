d82aed63e76bd6b60472695173aa679c6cf46ddd
James Bell <james@stellentus.com>
2018-04-24 11:21:58 -0600

    Refactor fillNaive

--- missing/fillNaive.m
+++ missing/fillNaive.m
@@ -1,15 +1,16 @@
 % fillNaive uses one of three naive strategies.
 % The three arguments provide different ways to initialize the NaN values. It doesn't make sense to set more than one of them.
 function [missingX] = fillNaive(missingX, completeX, missingMask, args)
+	mask = isnan(missingX);
 	if isfield(args, 'zeroNaN') && args.zeroNaN
-		missingX(isnan(missingX)) = 0;
+		missingX(mask) = 0;
 	end
 	if isfield(args, 'meanNaN') && args.meanNaN
 		meanX = ones(size(missingX, 1), 1)*nanmean([missingX; completeX]);
-		missingX(isnan(missingX)) = meanX(isnan(missingX));
+		missingX(mask) = meanX(mask);
 	end
 	if isfield(args, 'randomNaN') && args.randomNaN
 		randX = rand(size(missingX));
-		missingX(isnan(missingX)) = randX(isnan(missingX));
+		missingX(mask) = randX(mask);
 	end
 end
