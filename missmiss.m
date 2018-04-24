21a6c499e09a5effb6f312944c1b137bad3993be
James Bell <james@stellentus.com>
2018-04-24 11:20:50 -0600

    Shorten names of algorithms

--- missmiss.m
+++ missmiss.m
@@ -24,9 +24,9 @@ function [X, covr, verrs, cerrs, algs] = missmiss(iters, fixedSeed, displayPlot,
 	% Set up functions to iterate through
 	algs = [];
 	algs = [algs; struct('func', @fillCCA, 'name', 'Listwise', 'args', struct())];
-	algs = [algs; struct('func', @fillNaive, 'name', 'Mean-Fill', 'args', struct('meanNaN', true))];
+	algs = [algs; struct('func', @fillNaive, 'name', 'Mean', 'args', struct('meanNaN', true))];
 	algs = [algs; struct('func', @fillPCA, 'name', 'PCA', 'args', struct('k', 4, 'VariableWeights', 'variance', 'meanNaN',  true))];
-	algs = [algs; struct('func', @fillRegr, 'name', 'Regression', 'args', struct())];
+	algs = [algs; struct('func', @fillRegr, 'name', 'Regress', 'args', struct())];
 	algs = [algs; struct('func', @fillMI, 'name', '    MI', 'args', struct('number', 10, 'length', 100))];
 	algs = [algs; struct('func', @fillAutoencoder, 'name', 'AE', 'args', struct('nh', 6, 'useAll', true, 'meanNaN', true))];
 	algs = [algs; struct('func', @fillCascadeAuto, 'name', 'Cascade', 'args', struct('nh', 6, 'rho', 0.99, 'epsilon', 1e-7, 'epochs', 500))];
