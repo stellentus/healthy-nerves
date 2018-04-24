f871996cad54d20189e2393373d53daf9a3aeba0
James Bell <james@stellentus.com>
2018-04-24 15:03:08 -0600

    Fix MI spacing

--- missmiss.m
+++ missmiss.m
@@ -27,7 +27,7 @@ function [X, covr, verrs, cerrs, algs] = missmiss(iters, fixedSeed, displayPlot,
 	algs = [algs; struct('func', @fillNaive, 'name', 'Mean', 'args', struct('handleNaN', 'mean', 'useMissingMaskForNaNFill', true))];
 	algs = [algs; struct('func', @fillPCA, 'name', 'PCA', 'args', struct('k', 7, 'VariableWeights', 'variance', 'handleNaN',  'mean'))];
 	algs = [algs; struct('func', @fillRegr, 'name', 'Regress', 'args', struct())];
-	algs = [algs; struct('func', @fillMI, 'name', '    MI', 'args', struct('number', 10, 'length', 100))];
+	algs = [algs; struct('func', @fillMI, 'name', 'MI', 'args', struct('number', 10, 'length', 100))];
 	algs = [algs; struct('func', @fillAutoencoder, 'name', 'AE', 'args', struct('nh', 6, 'useAll', true, 'handleNaN', 'mean'))];
 	algs = [algs; struct('func', @fillCascadeAuto, 'name', 'Cascade', 'args', struct('nh', 6, 'rho', 0.99, 'epsilon', 1e-7, 'epochs', 500))];
 
