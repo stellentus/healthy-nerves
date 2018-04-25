a351e4d3913b0f7b28feb72a62ffa739258b0cc6
James Bell <james@stellentus.com>
2018-04-25 10:46:29 -0600

    Refactor AutoBlanker to use fillNaive

--- missing/fillAutoBlanker.m
+++ missing/fillAutoBlanker.m
@@ -1,27 +1,16 @@
 % fillAutoBlanker uses an autoencoder (but with a different number of inputs and outputs).
 function [filledX] = fillAutoBlanker(missingX, completeX, missingMask, arg)
-	[numSamplesMissing, numFeatures] = size(missingX);
-
-	model.params = arg;
-
-	allX = [completeX; missingX];
-	mn = nanmean(allX);
-	for j = 1:numFeatures
-		% Skip this column if it has no missing values
-		if sum(missingMask(:, j)) == 0
-			continue
-		end
-
-		for i = 1:size(allX, 1)
-			if isnan(allX(i, j))
-				allX(i, j) = mn(j);
-			end
-		end
+	if ~isfield(args, 'handleNaN')
+		ars.handleNaN = 'mean';
 	end
+
+	missingX = fillNaive(missingX, completeX, missingMask, args);
+	allX = [completeX; missingX];
 
 	addpath ./algorithm
 
 	% Train
+	model.params = arg;
 	model = neuralnetwork(model);
 	model = neuralnetwork(model, allX, allX);
 
