190b0d386ba05590c2fecd1f8ceb1696159d8531
James Bell <james@stellentus.com>
2018-04-25 10:52:12 -0600

    Improve behavior of auto-iterator

--- missing/fillAutoIter.m
+++ missing/fillAutoIter.m
@@ -3,23 +3,18 @@ function [filledX] = fillAutoIter(missingX, completeX, missingMask, arg)
 	if ~isfield(arg, 'epochs')
 		arg.epochs = 10;
 	end
-	model.params = arg;
 
 	% If the appropriate flag is set, this function will fill NaN with some naive value. Otherwise, it does nothing.
-	missingX = fillNaive(missingX, completeX, missingMask, arg);
+	filledX = fillNaive(missingX, completeX, missingMask, arg);
 
-	addpath ./algorithm
-
-	model = neuralnetwork(model); % Set weights back to random
-
-	filledX = missingX;
-	for i=1:arg.epochs
-		trainData = [completeX; filledX]; % all data
-		model = neuralnetwork(model, trainData, trainData); % Train
-
-		model = neuralnetwork(model, filledX); % Predict
-		filledX = updateKnownValues(model.Y, missingX, missingMask); % Repair the known values from missingX
+	% Now make sure future iterations don't reset the NaN values
+	if isfield(arg, 'handleNaN')
+		arg.handleNaN = '';
 	end
 
-	rmpath ./algorithm
+	% Iterate through predictions
+	for i=1:arg.epochs
+		filledX = fillAutoencoder(filledX, completeX, missingMask, arg);
+		filledX = updateKnownValues(filledX, missingX, missingMask); % Repair the known values from missingX
+	end
 end
