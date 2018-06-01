e9bd277eebb34b66fac4ab7fb1c54e1617958de6
James Bell <james@stellentus.com>
2018-06-01 09:29:56 -0600

    Rename variables

--- missing/getCompleteIndices.m
+++ missing/getCompleteIndices.m
@@ -1,19 +1,19 @@
 % getCompleteIndices gets a list of the complete indices.
 % It can also return the list of indices with missing values and the number of missing values for each of them.
 function [completeIndices, missIndices, missCount] = getCompleteIndices(missingX, missingMask)
-	[numSamplesMissing, numFeatures] = size(missingX);
+	[numRows, numColumns] = size(missingX);
 
 	missIndices = [];
 	missCount = [];
 	completeIndices = [];
-	for j = 1:numFeatures
+	for ind = 1:numColumns
 		% Skip this column if it has no missing values
-		numColumnPresent = sum(missingMask(:, j));
-		if numColumnPresent == numSamplesMissing
-			completeIndices = [completeIndices; j];
+		countOfPresentValues = sum(missingMask(:, ind)); % Count the number that aren't mimssing
+		if countOfPresentValues == numRows
+			completeIndices = [completeIndices; ind];
 		else
-			missIndices = [missIndices; j];
-			missCount = [missCount; numSamplesMissing - numColumnPresent];
+			missIndices = [missIndices; ind];
+			missCount = [missCount; numRows - countOfPresentValues];
 		end
 	end
 end
