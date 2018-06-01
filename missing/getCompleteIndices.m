65de98d3aaaea89b89cae76ee1e5442c3a76958a
James Bell <james@stellentus.com>
2018-06-01 09:34:08 -0600

    Allow getCompleteIndices to operate on rows or columns

--- missing/getCompleteIndices.m
+++ missing/getCompleteIndices.m
@@ -1,19 +1,37 @@
 % getCompleteIndices gets a list of the complete indices.
 % It can also return the list of indices with missing values and the number of missing values for each of them.
-function [completeIndices, missIndices, missCount] = getCompleteIndices(missingX, missingMask)
+% By default it operates on columns, but the third argument can be used to change that.
+function [completeIndices, missIndices, missCount] = getCompleteIndices(missingX, missingMask, dim)
+	if nargin < 3
+		dim = 2
+	end
+
 	[numRows, numColumns] = size(missingX);
+
+	if dim == 1
+		numToIterate = numRows;
+		maxCount = numColumns;
+	else
+		numToIterate = numColumns;
+		maxCount = numRows;
+	end
 
 	missIndices = [];
 	missCount = [];
 	completeIndices = [];
-	for ind = 1:numColumns
+	for ind = 1:numToIterate
 		% Skip this column if it has no missing values
-		countOfPresentValues = sum(missingMask(:, ind)); % Count the number that aren't mimssing
-		if countOfPresentValues == numRows
+		if dim == 1
+			countOfPresentValues = sum(missingMask(ind, :)); % Count the number that aren't missing
+		else
+			countOfPresentValues = sum(missingMask(:, ind)); % Count the number that aren't missing
+		end
+
+		if countOfPresentValues == maxCount
 			completeIndices = [completeIndices; ind];
 		else
 			missIndices = [missIndices; ind];
-			missCount = [missCount; numRows - countOfPresentValues];
+			missCount = [missCount; maxCount - countOfPresentValues];
 		end
 	end
 end
