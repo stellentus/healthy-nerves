b3745c51b66e8771dfaabcaf13542cf1b733904b
James Bell <james@stellentus.com>
2018-06-06 06:09:03 -0600

    Revert "Add another example"
    This reverts commit e079011c134bb241e6318b5243bd4f68997f7194.
    It wasn't supposed to be in this project.

--- addPlotPoint3.m
+++
@@ -1,8 +0,0 @@
-function addPlotPoint3(axis, hit)
-    a = hit.IntersectionPoint;
-    h = findobj(axis,'Type','line');
-    x=get(h,'Xdata');
-    y=get(h,'Ydata');
-    cla(axis);
-    plot([x a(1)], [y a(2)], 'd')
-end
