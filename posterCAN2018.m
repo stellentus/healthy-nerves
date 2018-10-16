21f98c8d8464a8179baef9557febb5f8821535d2
James Bell <james@stellentus.com>
2018-10-16 14:18:55 -0600

    Add comments

--- posterCAN2018.m
+++ posterCAN2018.m
@@ -1,14 +1,20 @@
 % posterCAN2018 creates figures for my CAN Symposium 2018 poster.
 function posterCAN2018()
 	addpath import;
+
+	% Load data
 	[cData, cParticipantNames, cMeasureNames] = mefimport(pathFor('leg'));
 	[mData, mParticipantNames, mMeasureNames] = mefimport(pathFor('arm'));
 
+
+	% Delete measures and participants unique to one dataset
 	[cData, mData, participants, measures] = retainMatchingParticipants(cData, cParticipantNames, cMeasureNames, mData, mParticipantNames, mMeasureNames);
 	clear cParticipantNames cMeasureNames mParticipantNames mMeasureNames;
 
+	% Delete NaN rows (i.e. participants with missing values)
 	[participants, cData, mData] = deleteNaN(participants, cData, mData);
 	clear participants;
+
 	rmpath import;
 
 	addpath analyze;
