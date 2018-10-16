e3fac7bcf60a5db5d9bca379255e7a9a34610251
James Bell <james@stellentus.com>
2018-10-16 14:15:18 -0600

    Split importTwo into retainMatchingParticipants

---
+++ import/retainMatchingParticipants.m
@@ -0,0 +1,34 @@
+% retainMatchingParticipants only keeps particpants and measures matching between both inputs
+function [cData, mData, participants, measures] = retainMatchingParticipants(cData, cParticipantNames, cMeasureNames, cStats, mData, mParticipantNames, mMeasureNames, mStats)
+	% Verify participant names are the same; then only save one list
+	indices = verifyNames(cParticipantNames, mParticipantNames);
+	[participants, cData, mData] = deleteRows(indices, cParticipantNames, cData, mData);
+	clear cParticipantNames mParticipantNames indices;
+
+	% Verify measure names are the same; then only save one list
+	indices = verifyNames(cMeasureNames, mMeasureNames);
+	[measures, cData, mData] = deleteColumns(indices, cMeasureNames, cData, mData);
+	clear cMeasureNames mMeasureNames indices;
+end
+
+function [indices, list1] = verifyNames(list1, list2)
+	indices = true(length(list1), 1);
+	for i = 1:length(list1)
+		if list1(i) ~= list2
+			disp('ERROR: The names at index ' + string(i) + ' don''t match: "' + string(list1(i)) + '" and "' + string(list2(i)) + '".');
+			indices(i) = false;
+		end
+	end
+end
+
+function [participants, data1, data2] = deleteRows(indices, participants, data1, data2)
+	data1 = data1(indices, :);
+	data2 = data2(indices, :);
+	participants = participants(indices);
+end
+
+function [measures, data1, data2] = deleteColumns(indices, measures, data1, data2)
+	data1 = data1(:, indices);
+	data2 = data2(:, indices);
+	measures = measures(indices);
+end
