afc32022616bb325f19607e13838480bb152001b
James Bell <james@stellentus.com>
2019-01-08 15:52:17 -0700

    Remove duplicate participants

--- batcher.m
+++ batcher.m
@@ -90,8 +90,15 @@ function [flatVals, flatParts] = flattenStructs(structVals, structParts)
 	flatParts = [];
 	fields = fieldnames(structVals);
 	for i = 1:numel(fields)
-		flatVals = [flatVals; structVals.(fields{i})];
-		flatParts = [flatParts; structParts.(fields{i})];
+		thisParts = structParts.(fields{i});
+		thisVals = structVals.(fields{i});
+		for j = 1:length(thisParts)
+			% Only add the data if the participant hasn't already been added
+			if length(flatParts) < 2 || sum(ismember(flatParts, thisParts(j))) == 0
+				flatVals = [flatVals; thisVals(j, :)];
+				flatParts = [flatParts; thisParts(j)];
+			end
+		end
 	end
 end
 
