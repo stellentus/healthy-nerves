6665cb6663488c0b78adb839f14b2807818920d4
James Bell <james@bellstone.ca>
2019-02-14 14:35:21 -0700

    Improve calculation of zeroMeanUnitVar

--- batcher.m
+++ batcher.m
@@ -14,11 +14,11 @@ function [brs] = batcher()
 	values = [canValues; japValues; porValues];
 	groupMean = mean(values);
 	groupStd = std(values);
-	values = zeroMeanUnitVar(values, groupMean, groupStd);
-	canValues = zeroMeanUnitVar(canValues, groupMean, groupStd);
-	japValues = zeroMeanUnitVar(japValues, groupMean, groupStd);
-	porValues = zeroMeanUnitVar(porValues, groupMean, groupStd);
-	legValues = zeroMeanUnitVar(legValues, groupMean, groupStd);
+	valuesShifted = zeroMeanUnitVar(values, groupMean, groupStd);
+	canShifted = zeroMeanUnitVar(canValues, groupMean, groupStd);
+	japShifted = zeroMeanUnitVar(japValues, groupMean, groupStd);
+	porShifted = zeroMeanUnitVar(porValues, groupMean, groupStd);
+	legShifted = zeroMeanUnitVar(legValues, groupMean, groupStd);
 
 	% Calculate and print measures of batching
 	iters = 30;
@@ -28,27 +28,28 @@ function [brs] = batcher()
 	brs = struct('str', [], 'cri_mean', [], 'cri_std', [], 'nmi_mean', [], 'nmi_std', []);
 
 	% Test the normative data
-	brs = getBatchResults(brs, "Normative data", iters, scurr.Seed, @kmeans, 3, values, labels);
+	brs = getBatchResults(brs, "Normative data", iters, scurr.Seed, @kmeans, 3, valuesShifted, labels);
 
 	% Remove each type to see how things change
-	% brs = getBatchResults(brs, "No Canada", iters, scurr.Seed, @kmeans, 2, [japValues; porValues], [ones(japNum, 1); ones(porNum, 1) * 2]);
-	% brs = getBatchResults(brs, "No Japan", iters, scurr.Seed, @kmeans, 2, [canValues; porValues], [ones(canNum, 1); ones(porNum, 1) * 2]);
-	% brs = getBatchResults(brs, "No Portugal", iters, scurr.Seed, @kmeans, 2, [canValues; japValues], [ones(canNum, 1); ones(japNum, 1) * 2]);
+	% Conclusion with normalization: There isn't a batch between J and P, there might be one between C and P, and there's definitely one between C and J. But in all cases it's small.
+	% brs = getBatchResults(brs, "No Canada", iters, scurr.Seed, @kmeans, 2, [japShifted; porShifted], [ones(japNum, 1); ones(porNum, 1) * 2]);
+	% brs = getBatchResults(brs, "No Japan", iters, scurr.Seed, @kmeans, 2, [canShifted; porShifted], [ones(canNum, 1); ones(porNum, 1) * 2]);
+	% brs = getBatchResults(brs, "No Portugal", iters, scurr.Seed, @kmeans, 2, [canShifted; japShifted], [ones(canNum, 1); ones(japNum, 1) * 2]);
 
 	% Confirm that random and perfectly batched data work as expected
-	% brs = getBatchResults(brs, "Random labels", iters, scurr.Seed, @kmeans, 3, values);
+	% brs = getBatchResults(brs, "Random labels", iters, scurr.Seed, @kmeans, 3, valuesShifted);
 	% brs = getBatchResults(brs, "Batched data", iters, scurr.Seed, @kmeans, 3, length(labels)); % Instead of passing any data at all, request both arrays to be identical random indices.
 
 	% Show larger batches with CP instead of median
-	brs = getBatchResults(brs, "Canadian legs", iters, scurr.Seed, @kmeans, 3, [legValues; japValues; porValues], [ones(legNum, 1); ones(japNum, 1) * 2; repmat(3, porNum, 1)]);
+	brs = getBatchResults(brs, "Canadian legs", iters, scurr.Seed, @kmeans, 3, [legShifted; japShifted; porShifted], [ones(legNum, 1); ones(japNum, 1) * 2; repmat(3, porNum, 1)]);
 
 	% Look at batches when RC is shifted logarithmically in time.
-	% Conclusion: Shifting everything right causes an increase in both measures, while shifting left causes a decrease. This may be due to the increase in magnitudes. (Perhaps this means RC is actually different between groups, but it only becomes dominant as RC values get larger relative to others. If so, I should scale all measures to have unit variance.)
-	% However, even though shifting everything causes an unexpected change, it's not significant, while the increase when only shifting Canada is significant.
-	brs = getBatchResults(brs, "All shifted right RC", iters, scurr.Seed, @kmeans, 3, shiftRightRC(values), labels);
-	brs = getBatchResults(brs, "Canada shifted right RC", iters, scurr.Seed, @kmeans, 3, [shiftRightRC(canValues); japValues; porValues], labels);
-	brs = getBatchResults(brs, "All shifted left RC", iters, scurr.Seed, @kmeans, 3, shiftLeftRC(values), labels);
-	brs = getBatchResults(brs, "Canada shifted left RC", iters, scurr.Seed, @kmeans, 3, [shiftLeftRC(canValues); japValues; porValues], labels);
+	% Conclusion without normalization: Shifting everything right causes an increase in both measures, while shifting left causes a decrease. This may be due to the increase in magnitudes. (Perhaps this means RC is actually different between groups, but it only becomes dominant as RC values get larger relative to others. If so, I should scale all measures to have unit variance.) However, even though shifting everything causes an unexpected change, it's not significant, while the increase when only shifting Canada is significant.
+	% Conclusion with normalization: shifting to the right causes an increase, but not to the left. The change only appears in the Canada shift, as expected.
+	brs = getBatchResults(brs, "All shifted right RC", iters, scurr.Seed, @kmeans, 3, zeroMeanUnitVar(shiftRightRC(values), groupMean, groupStd), labels);
+	brs = getBatchResults(brs, "Canada shifted right RC", iters, scurr.Seed, @kmeans, 3, [zeroMeanUnitVar(shiftRightRC(canValues), groupMean, groupStd); japShifted; porShifted], labels);
+	brs = getBatchResults(brs, "All shifted left RC", iters, scurr.Seed, @kmeans, 3, zeroMeanUnitVar(shiftLeftRC(values), groupMean, groupStd), labels);
+	brs = getBatchResults(brs, "Canada shifted left RC", iters, scurr.Seed, @kmeans, 3, [zeroMeanUnitVar(shiftLeftRC(canValues), groupMean, groupStd); japShifted; porShifted], labels);
 
 	printBatchResults(brs);
 end
