0c7dd847c9e632bdc509c99ded8fd7e95c4d9eba
James Bell <james@bellstone.ca>
2019-02-15 15:18:16 -0700

    Plot variance as violins

--- variance.m
+++ variance.m
@@ -6,6 +6,17 @@ function [brs] = variance()
 	printTableOfVals("Variance for Measure", strs, std([canValues; japValues; porValues]), std(canValues), std(japValues), std(porValues));
 	disp(" ");
 	printTableOfVals("Mean for Measure", strs, mean([canValues; japValues; porValues]), mean(canValues), mean(japValues), mean(porValues));
+
+	displayMeasure(1, measures, [canValues; japValues; porValues], canValues, japValues, porValues);
+	displayMeasure(2, measures, [canValues; japValues; porValues], canValues, japValues, porValues);
+	displayMeasure(3, measures, [canValues; japValues; porValues], canValues, japValues, porValues);
+	displayMeasure(5, measures, [canValues; japValues; porValues], canValues, japValues, porValues);
+	displayMeasure(14, measures, [canValues; japValues; porValues], canValues, japValues, porValues);
+	displayMeasure(20, measures, [canValues; japValues; porValues], canValues, japValues, porValues);
+	displayMeasure(21, measures, [canValues; japValues; porValues], canValues, japValues, porValues);
+	displayMeasure(25, measures, [canValues; japValues; porValues], canValues, japValues, porValues);
+	displayMeasure(26, measures, [canValues; japValues; porValues], canValues, japValues, porValues);
+	displayMeasure(29, measures, [canValues; japValues; porValues], canValues, japValues, porValues);
 end
 
 function [prc] = toPerc(thisS, allS)
@@ -18,4 +29,26 @@ function printTableOfVals(name, strs, comb, can, jap, por)
 	for i=1:length(strs)
 		fprintf('%s | % 8.3f | % 8.3f (% 3.0f) | % 8.3f (% 3.0f) | % 8.3f (% 3.0f) \n', strs(i), comb(i), can(i), toPerc(can(i),comb(i)), jap(i), toPerc(jap(i),comb(i)), por(i), toPerc(por(i),comb(i)));
 	end
+end
+
+function displayMeasure(index, measures, comb, can, jap, por)
+	maxLen = max(size(comb, 1), max(size(can, 1), max(size(jap, 1), size(por, 1))));
+	vals = nan(maxLen, 4);
+	vals(1:size(comb, 1), 1) = comb(:, index);
+	vals(1:size(can, 1), 2) = can(:, index);
+	vals(1:size(jap, 1), 3) = jap(:, index);
+	vals(1:size(por, 1), 4) = por(:, index);
+
+	addpath lib;
+
+	pathstr = sprintf('img/norm-var-idx%02d-%d-%d-%d-%d%d%2.0f', index, clock);
+
+	fig = figure('DefaultAxesFontSize', 18);
+	violin(vals, 'xlabel', {'All', 'Canada', 'Japan', 'Portugal'});
+	title(['Comparison of' measures(index)]);
+	savefig(fig, strcat(pathstr, '.fig', 'compact'));
+	saveas(fig, strcat(pathstr, '.png'));
+	saveas(fig, sprintf('img/variance-%02d.png',index)); % No timestamp so it's possible to keep the same image open
+
+	rmpath lib;
 end
