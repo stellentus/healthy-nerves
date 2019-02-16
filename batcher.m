18bde48ff6ee3b3d35e62b36d049f27b4914beb2
James Bell <james@bellstone.ca>
2019-02-15 20:47:02 -0700

    Improve organization of batcher

--- batcher.m
+++ batcher.m
@@ -17,41 +17,41 @@ function [brs] = batcher()
 
 	addpath analyze;
 
-	bas = [];
+	bas = [
+		% Test the normative data
+		BatchAnalyzer("Normative data", iters, 3, values, labels);
 
-	% Test the normative data
-	bas = [bas BatchAnalyzer("Normative data", iters, 3, values, labels)];
+		% Remove each type to see how things change
+		% Conclusion with normalization: There isn't a batch between J and P, there might be one between C and P, and there's definitely one between C and J. But in all cases it's small.
+		BatchAnalyzer("No Canada", iters, 2, [japValues; porValues], [ones(japNum, 1); ones(porNum, 1) * 2]);
+		BatchAnalyzer("No Japan", iters, 2, [canValues; porValues], [ones(canNum, 1); ones(porNum, 1) * 2]);
+		BatchAnalyzer("No Portugal", iters, 2, [canValues; japValues], [ones(canNum, 1); ones(japNum, 1) * 2]);
 
-	% Remove each type to see how things change
-	% Conclusion with normalization: There isn't a batch between J and P, there might be one between C and P, and there's definitely one between C and J. But in all cases it's small.
-	bas = [bas BatchAnalyzer("No Canada", iters, 2, [japValues; porValues], [ones(japNum, 1); ones(porNum, 1) * 2])];
-	bas = [bas BatchAnalyzer("No Japan", iters, 2, [canValues; porValues], [ones(canNum, 1); ones(porNum, 1) * 2])];
-	bas = [bas BatchAnalyzer("No Portugal", iters, 2, [canValues; japValues], [ones(canNum, 1); ones(japNum, 1) * 2])];
+		% Confirm that random and perfectly batched data work as expected
+		BatchAnalyzer("Random labels", iters, 3, values);
+		% BatchAnalyzer("Batched data", iters, 3, length(labels)); % Instead of passing any data at all, request both arrays to be identical random indices;
 
-	% Confirm that random and perfectly batched data work as expected
-	bas = [bas BatchAnalyzer("Random labels", iters, 3, values)];
-	% bas = [bas BatchAnalyzer("Batched data", iters, 3, length(labels)); % Instead of passing any data at all, request both arrays to be identical random indices].
+		% Show larger batches with CP instead of median
+		BatchAnalyzer("Canadian legs", iters, 3, [legValues; japValues; porValues], [ones(legNum, 1); ones(japNum, 1) * 2; repmat(3, porNum, 1)]);
 
-	% Show larger batches with CP instead of median
-	bas = [bas BatchAnalyzer("Canadian legs", iters, 3, [legValues; japValues; porValues], [ones(legNum, 1); ones(japNum, 1) * 2; repmat(3, porNum, 1)])];
+		% Look at batches when RC is shifted logarithmically in time.
+		% Conclusion without normalization: Shifting everything right causes an increase in both measures, while shifting left causes a decrease. This may be due to the increase in magnitudes. (Perhaps this means RC is actually different between groups, but it only becomes dominant as RC values get larger relative to others. If so, I should scale all measures to have unit variance.) However, even though shifting everything causes an unexpected change, it's not significant, while the increase when only shifting Canada is significant.
+		% Conclusion with normalization: shifting to the right causes an increase, but not to the left. The change only appears in the Canada shift, as expected.
+		BatchAnalyzer("All shifted right RC", iters, 3, shiftRightRC(values), labels);
+		BatchAnalyzer("Canada shifted right RC", iters, 3, [shiftRightRC(canValues); japValues; porValues], labels);
+		BatchAnalyzer("All shifted left RC", iters, 3, shiftLeftRC(values), labels);
+		BatchAnalyzer("Canada shifted left RC", iters, 3, [shiftLeftRC(canValues); japValues; porValues], labels);
 
-	% Look at batches when RC is shifted logarithmically in time.
-	% Conclusion without normalization: Shifting everything right causes an increase in both measures, while shifting left causes a decrease. This may be due to the increase in magnitudes. (Perhaps this means RC is actually different between groups, but it only becomes dominant as RC values get larger relative to others. If so, I should scale all measures to have unit variance.) However, even though shifting everything causes an unexpected change, it's not significant, while the increase when only shifting Canada is significant.
-	% Conclusion with normalization: shifting to the right causes an increase, but not to the left. The change only appears in the Canada shift, as expected.
-	bas = [bas BatchAnalyzer("All shifted right RC", iters, 3, shiftRightRC(values), labels)];
-	bas = [bas BatchAnalyzer("Canada shifted right RC", iters, 3, [shiftRightRC(canValues); japValues; porValues], labels)];
-	bas = [bas BatchAnalyzer("All shifted left RC", iters, 3, shiftLeftRC(values), labels)];
-	bas = [bas BatchAnalyzer("Canada shifted left RC", iters, 3, [shiftLeftRC(canValues); japValues; porValues], labels)];
+		% Conclusion: All shrunk has no impact because it's all changed to unit variance, but shrinking just Canada has a HUGE impact on clustering the Canadian data.
+		% BatchAnalyzer("All shrunk RC", iters, 3, shrinkRC(values), labels);
+		BatchAnalyzer("Canada shrunk RC", iters, 3, [shrinkRC(canValues); japValues; porValues], labels);
 
-	% Conclusion: All shrunk has no impact because it's all changed to unit variance, but shrinking just Canada has a HUGE impact on clustering the Canadian data.
-	% bas = [bas BatchAnalyzer("All shrunk RC", iters, 3, shrinkRC(values), labels)];
-	bas = [bas BatchAnalyzer("Canada shrunk RC", iters, 3, [shrinkRC(canValues); japValues; porValues], labels)];
-
-	% Conclusion: All reduced/increased has no impact because it's all changed to unit variance, but shrinking just Canada decreases the ARI/NMI (while increasing increases), suggesting the Canadian data has more variance than the others.
-	% bas = [bas BatchAnalyzer("All reduced variance", iters, 3, scaleVariance(values, 2.0), labels)];
-	bas = [bas BatchAnalyzer("Canada reduced variance", iters, 3, [scaleVariance(canValues, 2.0); japValues; porValues], labels)];
-	% bas = [bas BatchAnalyzer("All increased variance", iters, 3, scaleVariance(values, .5), labels)];
-	bas = [bas BatchAnalyzer("Canada increased variance", iters, 3, [scaleVariance(canValues, .5); japValues; porValues], labels)];
+		% Conclusion: All reduced/increased has no impact because it's all changed to unit variance, but shrinking just Canada decreases the ARI/NMI (while increasing increases), suggesting the Canadian data has more variance than the others.
+		% BatchAnalyzer("All reduced variance", iters, 3, scaleVariance(values, 2.0), labels);
+		BatchAnalyzer("Canada reduced variance", iters, 3, [scaleVariance(canValues, 2.0); japValues; porValues], labels);
+		% BatchAnalyzer("All increased variance", iters, 3, scaleVariance(values, .5), labels);
+		BatchAnalyzer("Canada increased variance", iters, 3, [scaleVariance(canValues, .5); japValues; porValues], labels);
+	];
 
 	num = length(bas);
 	str = strings(1,num);
