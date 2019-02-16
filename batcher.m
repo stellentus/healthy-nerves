31fcd7a7490caa05c574af9e6fd12d146a9f85d3
James Bell <james@bellstone.ca>
2019-02-15 20:35:21 -0700

    Refactor for parfor

--- batcher.m
+++ batcher.m
@@ -55,26 +55,31 @@ function [brs] = batcher()
 	bas = [bas BatchAnalyzer("Canada increased variance", iters, 3, [scaleVariance(canValues, .5); japValues; porValues], labels)];
 
 	num = length(bas);
-	brs = struct('str', strings(1,num), 'cri_mean', zeros(1,num), 'cri_std', zeros(1,num), 'nmi_mean', zeros(1,num), 'nmi_std', zeros(1,num));
+	str = strings(1,num);
+	cri_mean = zeros(1,num);
+	cri_std = zeros(1,num);
+	nmi_mean = zeros(1,num);
+	nmi_std = zeros(1,num);
+
 	for i = 1:num
 		ba = bas(i);
 		calculateBatch(ba);
-		brs.str(i) = ba.Name;
-		brs.cri_mean(i) = mean(ba.CRI);
-		brs.cri_std(i) = std(ba.CRI);
-		brs.nmi_mean(i) = mean(ba.NMI);
-		brs.nmi_std(i) = std(ba.NMI);
+		str(i) = ba.Name;
+		cri_mean(i) = mean(ba.CRI);
+		cri_std(i) = std(ba.CRI);
+		nmi_mean(i) = mean(ba.NMI);
+		nmi_std(i) = std(ba.NMI);
 	end
 
-	printBatchResults(brs);
+	printBatchResults(str, cri_mean, cri_std, nmi_mean, nmi_std);
 end
 
-function printBatchResults(brs)
-	strs = pad(brs.str);
+function printBatchResults(str, cri_mean, cri_std, nmi_mean, nmi_std)
+	strs = pad(str);
 	fprintf('%s |  CRI (std)     |  NMI (std)     \n', pad("Name", strlength(strs(1))));
 	fprintf('%s | -------------- | -------------- \n', strrep(pad(" ", strlength(strs(1))), " ", "-"));
 	for i=1:length(strs)
-		fprintf('%s | % .3f (%.3f) | % .3f (%.3f) \n', strs(i), brs.cri_mean(i), brs.cri_std(i), brs.nmi_mean(i), brs.nmi_std(i));
+		fprintf('%s | % .3f (%.3f) | % .3f (%.3f) \n', strs(i), cri_mean(i), cri_std(i), nmi_mean(i), nmi_std(i));
 	end
 end
 
