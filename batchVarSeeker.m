d0e700c0a412b9bb32aa7339dff4e56baad6bdd7
James Bell <james@bellstone.ca>
2019-02-19 15:29:25 -0700

    Scale the variance by the number of columns being modified

--- batchVarSeeker.m
+++ batchVarSeeker.m
@@ -12,16 +12,16 @@ function [brs] = batchVarSeeker()
 	ba = BatchAnalyzer('Normative', iters, 3, [canValues; japValues; porValues], labels);
 	bas = [ba];
 	for i = [1:length(measures)]
-		bas = [bas BACopyWithValues(ba, sprintf('Decrease %s', measures(i)), [scaleVariance(canValues, 0.5, i); japValues; porValues])];
-		bas = [bas BACopyWithValues(ba, sprintf('Increase %s', measures(i)), [scaleVariance(canValues, 2.0, i); japValues; porValues])];
+		bas = [bas BACopyWithValues(ba, sprintf('Decrease %s', measures(i)), [scaleVariance(canValues, 0.1, i); japValues; porValues])];
+		bas = [bas BACopyWithValues(ba, sprintf('Increase %s', measures(i)), [scaleVariance(canValues, 10, i); japValues; porValues])];
 	end
 	for plt = ["SR" "IV" "QT" "TEd" "TEh" "Meta" "RC" "all"];
-		bas = [bas BACopyWithValues(ba, sprintf('Decrease %s plot', plt), [scaleVariance(canValues, 0.5, indicesForPlot(plt)); japValues; porValues])];
-		bas = [bas BACopyWithValues(ba, sprintf('Increase %s plot', plt), [scaleVariance(canValues, 2.0, indicesForPlot(plt)); japValues; porValues])];
+		bas = [bas BACopyWithValues(ba, sprintf('Decrease %s plot', plt), [scaleVariance(canValues, 0.1, indicesForPlot(plt)); japValues; porValues])];
+		bas = [bas BACopyWithValues(ba, sprintf('Increase %s plot', plt), [scaleVariance(canValues, 10, indicesForPlot(plt)); japValues; porValues])];
 	end
 	for channel = ["Ci" "Cmy" "Gmy" "GKf" "GKfi" "GKir" "GKs" "GLk" "GLki" "PNa" "PNap" "vleak" "all"];
-		bas = [bas BACopyWithValues(ba, sprintf('Decrease %s channel', channel), [scaleVariance(canValues, 0.5, indicesForChannel(channel)); japValues; porValues])];
-		bas = [bas BACopyWithValues(ba, sprintf('Increase %s channel', channel), [scaleVariance(canValues, 2.0, indicesForChannel(channel)); japValues; porValues])];
+		bas = [bas BACopyWithValues(ba, sprintf('Decrease %s channel', channel), [scaleVariance(canValues, 0.1, indicesForChannel(channel)); japValues; porValues])];
+		bas = [bas BACopyWithValues(ba, sprintf('Increase %s channel', channel), [scaleVariance(canValues, 10, indicesForChannel(channel)); japValues; porValues])];
 	end
 
 	num = length(bas);
@@ -56,6 +56,8 @@ function printBatchResults(str, cri_mean, cri_std, nmi_mean, nmi_std)
 end
 
 function [vals] = scaleVariance(vals, stdScale, colInd)
+	% The variance is scaled by the number of columns being modified. This makes the overall change more comparable.
+	stdScale = (stdScale-1)/sqrt(length(colInd))+1;
 	mns = mean(vals(:,colInd));
 	vals(:,colInd) = bsxfun(@times, vals(:,colInd) - mns, stdScale) + mns;
 end
