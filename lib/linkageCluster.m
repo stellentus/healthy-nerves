4ee7daaa4848d3c061cb5c5de616644b9f351144
James Bell <james@bellstone.ca>
2019-02-21 15:24:33 -0700

    Add alternative linkage cluster options

--- lib/linkageCluster.m
+++ lib/linkageCluster.m
@@ -1,5 +1,11 @@
 function clusters = linkageCluster(values, groups)
 	% This is deterministic
+	% Z = linkage(values, 'single');
+	% Z = linkage(values, 'complete');
+	% Z = linkage(values, 'average');
+	% Z = linkage(values, 'weighted');
+	% Z = linkage(values, 'centroid'); % Warning: Non-monotonic cluster tree -- the centroid linkage is probably not appropriate.
+	% Z = linkage(values, 'median'); % Warning: Non-monotonic cluster tree -- the median linkage is probably not appropriate.
 	Z = linkage(values, 'ward');
 	clusters = cluster(Z, 'Maxclust', groups);
 end
