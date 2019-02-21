b9a0e3dcef95c4bb7258972659305f0c9b1330b0
James Bell <james@bellstone.ca>
2019-02-21 11:10:19 -0700

    Set default value for argument

--- batches/getDeletedFeatureBatches.m
+++ batches/getDeletedFeatureBatches.m
@@ -1,8 +1,15 @@
 %% getDeletedFeatureBatches returns a list of BatchAnalyzer that delete each feature.
 % These results somewhat point to SDTC, Hyperpol I/V slope, TEd(10-20ms), TEd(90-100ms), and Age, but it's not really significant.
-%% arg.toDelete is the number of indices to delete.
+%% arg.toDelete is the number of indices to delete (default 1).
 function [bas] = getDeletedFeatureBatches(iters, arg)
 	load('bin/batch-normative.mat');
+
+	if nargin < 2
+		arg = struct();
+	end
+	if ~isfield(arg, 'toDelete')
+		arg.toDelete = 1;
+	end
 
 	% Create a combined vector for labels (with all datasets)
 	labels = [ones(size(canValues, 1), 1); ones(size(japValues, 1), 1) * 2; repmat(3, size(porValues, 1), 1)];
