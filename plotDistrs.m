3018b0ad6730f7aad781def0eff9c43311f2b04c
James Bell <james@bellstone.ca>
2019-02-28 08:51:04 -0700

    Add code to show the distributions

---
+++ plotDistrs.m
@@ -0,0 +1,13 @@
+function plotDistrs()
+	load("bin/batch-normative.mat");
+	values = [canValues; japValues; porValues];
+
+	close all;
+
+	for i=1:size(values, 2)
+		fig = figure('DefaultAxesFontSize', 18);
+		histogram(values(:, i), 100);
+		title(measures(i));
+		% saveas(fig, sprintf('img/plotDistrs%02d-%02d-%02d-%02d-%02d%02d%2.0f.png', clock));
+	end
+end
