8c9f245d57eb2ab41d0c5d8b9b642aae7a13a0e2
James Bell <james@bellstone.ca>
2019-03-20 15:32:40 -0600

    Plot figure 1

--- batcherFigures.m
+++ batcherFigures.m
@@ -1,19 +1,37 @@
 % batcherFigures plots figures for the preliminary batch effect analysis.
 function batcherFigures()
-	iter = 30;
+	iters = 30;
 	sampleFraction = 0.8;
-	normativeFile = "bin/batch-normative.mat";
+
+	load("bin/batch-normative.mat");
+
+	canNum = size(canValues, 1);
+	japNum = size(japValues, 1);
+	porNum = size(porValues, 1);
+	legNum = size(legValues, 1);
+
+	% Create a combined vector for labels (with all datasets) and one for values
+	labels = [ones(canNum, 1); ones(japNum, 1) * 2; repmat(3, porNum, 1)];
+	values = [canValues; japValues; porValues];
 
 	addpath batches;
 
-	bas = getMiscSeekerBatches(iter, sampleFraction, normativeFile, false);
+	ba = BatchAnalyzer("Normative data", 3, values, labels, 'iters', iters, 'sampleFraction', sampleFraction);
+	bas = [
+		ba;
+		BatchAnalyzer("Random labels", 3, values, 'iters', iters, 'sampleFraction', sampleFraction);
+	];
 
+	calcAndPlot(bas, 'batch-f1');
+
+	rmpath batches;
+end
+
+function calcAndPlot(bas, name)
 	% Calculate BE and print
 	for i = 1:length(bas)
 		calculateBatch(bas(i));
 	end
 
-	plotBas(bas, 'bfigs');
-
-	rmpath batches;
+	plotBas(bas, name);
 end
