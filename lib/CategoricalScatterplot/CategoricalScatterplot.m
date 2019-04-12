4ee6fb2231b29d1da7104418e594289b58c72720
James Bell <james@bellstone.ca>
2019-04-12 16:25:57 -0600

    Add Ladder Lines to Categorical Scatterplot

--- lib/CategoricalScatterplot/CategoricalScatterplot.m
+++ lib/CategoricalScatterplot/CategoricalScatterplot.m
@@ -81,6 +81,7 @@ addParameter(p, 'WhiskerLineStyle', '--', ...
 addParameter(p, 'BoxLineWidth', 1.0, @(x) isnumeric(x));
 addParameter(p, 'MedianLineWidth', 1.0, @(x) isnumeric(x));
 addParameter(p, 'WhiskerLineWidth', 1.0, @(x) isnumeric(x));
+addParameter(p, 'LadderLines', false, @(x) islogical(x));
 % Parse inputs and unpack structure
 parse(p, X, varargin{:});
 parsed = p.Results;
@@ -206,6 +207,13 @@ for i = 1:length(groups)
         else
             scatter(new_data{i,2}, new_data{i,1}, parsed.MarkerSize, parsed.Color(i,:));
         end
+    end
+
+    % Draw ladder lines, if necessary
+    if parsed.LadderLines && mod(i,2)==0
+        ladderColor = parsed.Color;
+        ladderColor(4) = 0.2;
+        plot([new_data{i-1,2}'; new_data{i,2}'], [new_data{i-1,1}'; new_data{i,1}'], 'Color', ladderColor);
     end
 
     % Draw median
