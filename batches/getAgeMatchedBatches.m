034d0a3c724650b41fb50f52a80b420e4cb28bd1
James Bell <james@bellstone.ca>
2019-04-17 15:10:46 -0600

    Fix bug in age matching

--- batches/getAgeMatchedBatches.m
+++ batches/getAgeMatchedBatches.m
@@ -8,13 +8,13 @@ function [bas] = getAgeMatchedBatches(iters, sampleFraction, filepath)
 	[canValues, japValues, porValues] = matchAges(canValues, japValues, porValues);
 
 	% Create a combined vector for labels (with all datasets) and one for values
-	labels = [ones(canNum, 1); ones(japNum, 1) * 2; repmat(3, porNum, 1)];
+	labels = [ones(size(canValues, 1), 1); ones(size(japValues, 1), 1) * 2; repmat(3, size(porValues, 1), 1)];
 	values = [canValues; japValues; porValues];
 
 	bas = [ba];
 
 	bas = [bas BatchAnalyzer("Age-matched data", 3, values, labels, 'iters', iters, 'sampleFraction', sampleFraction)];
-	bas = [bas BatchAnalyzer("No CA", 2, [japValues; porValues], [ones(japNum, 1); ones(porNum, 1) * 2], 'iters', iters, 'sampleFraction', sampleFraction)];
-	bas = [bas BatchAnalyzer("No JA", 2, [canValues; porValues], [ones(canNum, 1); ones(porNum, 1) * 2], 'iters', iters, 'sampleFraction', sampleFraction)];
-	bas = [bas BatchAnalyzer("No PO", 2, [canValues; japValues], [ones(canNum, 1); ones(japNum, 1) * 2], 'iters', iters, 'sampleFraction', sampleFraction)];
+	bas = [bas BatchAnalyzer("No CA", 2, [japValues; porValues], [ones(size(japValues, 1), 1); ones(size(porValues, 1), 1) * 2], 'iters', iters, 'sampleFraction', sampleFraction)];
+	bas = [bas BatchAnalyzer("No JA", 2, [canValues; porValues], [ones(size(canValues, 1), 1); ones(size(porValues, 1), 1) * 2], 'iters', iters, 'sampleFraction', sampleFraction)];
+	bas = [bas BatchAnalyzer("No PO", 2, [canValues; japValues], [ones(size(canValues, 1), 1); ones(size(japValues, 1), 1) * 2], 'iters', iters, 'sampleFraction', sampleFraction)];
 end
