6beba90f2b40224f51de3dd75ed858c7c8af6442
James Bell <james@bellstone.ca>
2019-04-24 07:26:24 -0600

    Import RC data

---
+++ import/mefRCimport.m
@@ -0,0 +1,32 @@
+% mefRCimport imports RC from the provided MEF file, only loading the provided participants, in order.
+function [delay, rc] = mefRCimport(filepath, participants)
+	% Import the raw data
+	[~, ~, raw] = xlsread(filepath, 'RC');
+
+	if nargin > 1
+		X = getColumns(raw, participants);
+		extractedParticipants = string(X(1, 2:size(X, 2)));
+		if 0 ~= sum(~strcmp(extractedParticipants', participants))
+			disp('WARNING: Participants were not loaded correctly');
+		end
+	else
+		X = raw(:, [1 8:end]);
+	end
+
+	lastParticipant = size(X, 2);
+
+	delay = cell2mat(X(3:20, 1));
+	rc = cell2mat(X(3:20, 2:lastParticipant));
+end
+
+function [X] = getColumns(raw, participants)
+	header = string(raw(1, :));
+
+	X = raw(:, 1);
+	for j = 1:length(participants)
+		ind = find(strcmp(header, participants(j)));
+		if ind
+			X = [X raw(:, ind)];
+		end
+	end
+end
