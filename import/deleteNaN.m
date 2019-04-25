8ae6f91fa936e3f315f77c1d3cb954f5e67ab0ea
James Bell <james@bellstone.ca>
2019-04-25 08:31:50 -0600

    Simplify deleteNaN

--- import/deleteNaN.m
+++ import/deleteNaN.m
@@ -1,15 +1,15 @@
 % deleteNaN deletes all NaN rows
 function [participants, data1Reduced, data2Reduced, indices] = deleteNaN(participants, data1, data2)
-	if nargin == 2
+	if nargin == 2  % data2 is not included (i.e. it's not both legs and arms for the same participants)
 		data2Reduced = [];
 
 		% Handle missing data by deleting NaN rows
-		indices = rowsWithNaN(participants, data1);
+		indices = rowsWithNaN(data1);
 		data1Reduced = data1(indices, :);
 		participants = participants(indices);
 	else
 		% Handle missing data by deleting NaN rows
-		indices = rowsWithNaN(participants, data1) & rowsWithNaN(participants, data2);
+		indices = rowsWithNaN(data1) & rowsWithNaN(data2);
 		participants = participants(indices);
 		data1Reduced = data1(indices, :);
 		data2Reduced = data2(indices, :);
@@ -17,7 +17,7 @@ function [participants, data1Reduced, data2Reduced, indices] = deleteNaN(partici
 end
 
 % rowsWithNaN returns any row with NaN.
-function [indices] = rowsWithNaN(participant, data1)
+function [indices] = rowsWithNaN(data1)
 	% Calculate which rows contain NaN for each type
 	indices = sum(isnan(data1), 2) == 0;
 end
