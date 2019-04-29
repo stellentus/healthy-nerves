90f5ef339d6ea27674cdb093adec5d53843d0a42
James Bell <james@bellstone.ca>
2019-04-29 13:16:41 -0600

    Stop printing countMissing outputs in scripts

--- missing/countMissing.m
+++ missing/countMissing.m
@@ -1,6 +1,7 @@
 % countMissing prints a count of the missing values in a dataset.
 % The provided filepath should contain measures, canValues, japValues, porValues, canNum, japNum, and porNum.
 % The output is ready to be pasted into a LaTeX table, but is also human-readable.
+% If the output variable 'nanPercent' is unused, the results are instead printed.
 function [nanPercent] = countMissing(filepath)
 	load(filepath);
 
@@ -15,29 +16,31 @@ function [nanPercent] = countMissing(filepath)
 	% Sort the missing allValues by frequency.
 	[~, idx] = sort(allNan, 'descend');
 
-	% Fix formatting for LaTeX output
-	measures = strrep(measures, "%", "\%");
-	measures = strrep(measures, "( ", "(");
+	if nargout == 0
+		% Fix formatting for LaTeX output
+		measures = strrep(measures, "%", "\%");
+		measures = strrep(measures, "( ", "(");
 
-	% Get length of longest string for padding purposes
-	padLen = max(strlength(measures(allNan~=0)));
+		% Get length of longest string for padding purposes
+		padLen = max(strlength(measures(allNan~=0)));
 
-	rowLen = 11;
-	fprintf('%s & %s & %s & %s & %s \n', pad("Measure Name", padLen), pad("CA", rowLen), pad("JP", rowLen), pad("PO", rowLen), pad("ALL", rowLen));
-	spacingString = strrep(pad(" ", rowLen), " ", "-");
-	fprintf('%s & %s & %s & %s & %s \n', strrep(pad(" ", padLen), " ", "-"), spacingString, spacingString, spacingString, spacingString);
-	for i=idx
-		if allNan(i) == 0
-			break;
+		rowLen = 11;
+		fprintf('%s & %s & %s & %s & %s \n', pad("Measure Name", padLen), pad("CA", rowLen), pad("JP", rowLen), pad("PO", rowLen), pad("ALL", rowLen));
+		spacingString = strrep(pad(" ", rowLen), " ", "-");
+		fprintf('%s & %s & %s & %s & %s \n', strrep(pad(" ", padLen), " ", "-"), spacingString, spacingString, spacingString, spacingString);
+		for i=idx
+			if allNan(i) == 0
+				break;
+			end
+			canMissStr = missString(canNan(i), canNum);
+			japMissStr = missString(japNan(i), japNum);
+			porMissStr = missString(porNan(i), porNum);
+			allMissStr = missString(allNan(i), allNum);
+			fprintf('%s & %s & %s & %s & %s \\\\\n', pad(measures(i), padLen), canMissStr, japMissStr, porMissStr, allMissStr);
 		end
-		canMissStr = missString(canNan(i), canNum);
-		japMissStr = missString(japNan(i), japNum);
-		porMissStr = missString(porNan(i), porNum);
-		allMissStr = missString(allNan(i), allNum);
-		fprintf('%s & %s & %s & %s & %s \\\\\n', pad(measures(i), padLen), canMissStr, japMissStr, porMissStr, allMissStr);
+	else
+		nanPercent = allNan/allNum;
 	end
-
-	nanPercent = allNan/allNum;
 end
 
 function [str] = missString(numNan, numTot)
