4d2dd8d9318a985d7e7d8c2a5553cc21aceac855
James Bell <james@bellstone.ca>
2019-05-20 16:01:12 -0600

    Change plots to what Kelvin prefers

--- pairs/plotBatches.m
+++ pairs/plotBatches.m
@@ -9,21 +9,15 @@ function plotBatches()
 
 	fig = figure('DefaultAxesFontSize', 18, 'Position', [10 10 900 600]);
 
-	subplot(2, 3, 1)
-	plotPair("Threshold Electrotonus", values, labels, measures, 11, 18);
-	subplot(2, 3, 2)
-	plotPair("Recovery Cycle", values, labels, measures, 29, 13);
-	subplot(2, 3, 3)
+	subplot(2, 2, 1)
+	plotPair("Threshold Electrotonus", values, labels, measures, 10, 18);
+	subplot(2, 2, 2)
+	plotPair("Recovery Cycle", values, labels, measures, 12, 13);
+	subplot(2, 2, 3)
 	plotPair("Charge Duration", values, labels, measures, 3, 2);
 	ylabel("SDTC (ms)");
-	subplot(2, 3, 4)
+	subplot(2, 2, 4)
 	plotPair("Threshold I/V", values, labels, measures, 6, 25);
-	subplot(2, 3, 5)
-	plotPair("Stimulus Response", values, labels, measures, 5, 1);
-	ylabel("Stim for 50% max");
-	subplot(2, 3, 6)
-	plotPair("Other", values, labels, measures, 8, 14);
-	xlabel("Temperature (C)");
 
 	savefig(fig, strcat(pathstr, '.fig'), 'compact');
 	saveas(fig, strcat(pathstr, '.png'));
