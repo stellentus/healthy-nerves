f9bd2fdc4384deed6c74996288ee6aec0a6a0cc6
James Bell <james@bellstone.ca>
2019-05-20 15:08:01 -0600

    Plot scatters as a single plot

--- pairs/plotBatches.m
+++ pairs/plotBatches.m
@@ -3,10 +3,29 @@ function plotBatches()
 	labels = [ones(canNum, 1); ones(japNum, 1) * 2; repmat(3, porNum, 1)];
 	values = [canValues; japValues; porValues];
 
+	filename = sprintf("scatters");
+	[~,~] = mkdir('img/pairs'); % Read and ignore returns to suppress warning if dir exists.
+	pathstr = sprintf('img/pairs/%s-%d-%d-%d-%d%d%2.0f', filename, clock);
+
+	fig = figure('DefaultAxesFontSize', 18, 'Position', [10 10 900 600]);
+
+	subplot(2, 3, 1)
 	plotPair("Threshold Electrotonus", values, labels, measures, 11, 18);
+	subplot(2, 3, 2)
 	plotPair("Recovery Cycle", values, labels, measures, 29, 13);
+	subplot(2, 3, 3)
 	plotPair("Charge Duration", values, labels, measures, 3, 2);
+	ylabel("SDTC (ms)");
+	subplot(2, 3, 4)
 	plotPair("Threshold I/V", values, labels, measures, 6, 25);
+	subplot(2, 3, 5)
 	plotPair("Stimulus Response", values, labels, measures, 5, 1);
+	ylabel("Stim for 50% max");
+	subplot(2, 3, 6)
 	plotPair("Other", values, labels, measures, 8, 14);
+	xlabel("Temperature (C)");
+
+	savefig(fig, strcat(pathstr, '.fig'), 'compact');
+	saveas(fig, strcat(pathstr, '.png'));
+	copyfile(strcat(pathstr, '.png'), strcat('img/pairs/', filename, '.png')); % Also save without timestamp
 end
--- pairs/plotPair.m
+++ pairs/plotPair.m
@@ -1,24 +1,35 @@
-function plotPair(name, values, labels, measures, idx1, idx2)
-	filename = sprintf("scatter-%d-%d", idx1, idx2);
+function plotPair(name, values, labels, measures, idx1, idx2, newFig)
+	if nargin < 7
+		newFig = false;
+	end
 
-	[~,~] = mkdir('img/pairs'); % Read and ignore returns to suppress warning if dir exists.
-	pathstr = sprintf('img/pairs/%s-%d-%d-%d-%d%d%2.0f', filename, clock);
+	if newFig
+		filename = sprintf("scatter-%d-%d", idx1, idx2);
 
-	fig = figure('DefaultAxesFontSize', 18, 'Position', [10 10 900 600]);
+		[~,~] = mkdir('img/pairs'); % Read and ignore returns to suppress warning if dir exists.
+		pathstr = sprintf('img/pairs/%s-%d-%d-%d-%d%d%2.0f', filename, clock);
+
+		fig = figure('DefaultAxesFontSize', 18, 'Position', [10 10 900 600]);
+		markSize = 30;
+	else
+		markSize = 15;
+	end
 
 	colors = [193,27,36; 0,73,137; 43,111,0]/256;
 
 	hold on;
 	for col=1:3
 		val = values(labels == col, :);
-		plot(val(:, idx1), val(:, idx2), '.', 'color', colors(col, :), 'MarkerSize', 30);
+		plot(val(:, idx1), val(:, idx2), '.', 'color', colors(col, :), 'MarkerSize', markSize);
 	end
 
 	title(name);
 	xlabel(measures(idx1));
 	ylabel(measures(idx2));
 
-	savefig(fig, strcat(pathstr, '.fig'), 'compact');
-	saveas(fig, strcat(pathstr, '.png'));
-	copyfile(strcat(pathstr, '.png'), strcat('img/pairs/', filename, '.png')); % Also save without timestamp
+	if newFig
+		savefig(fig, strcat(pathstr, '.fig'), 'compact');
+		saveas(fig, strcat(pathstr, '.png'));
+		copyfile(strcat(pathstr, '.png'), strcat('img/pairs/', filename, '.png')); % Also save without timestamp
+	end
 end
