5e1a6337c056ef8739e64822d32a2b1c7b45174c
James Bell <james@bellstone.ca>
2019-05-21 16:47:00 -0600

    Add CI to scatter plot

--- pairs/plotPair.m
+++ pairs/plotPair.m
@@ -12,7 +12,7 @@ function plotPair(name, values, labels, measures, idx1, idx2, newFig)
 		fig = figure('DefaultAxesFontSize', 18, 'Position', [10 10 900 600]);
 		markSize = 30;
 	else
-		markSize = 15;
+		markSize = 10;
 	end
 
 	colors = [193,27,36; 0,73,137; 43,111,0]/256;
@@ -20,8 +20,16 @@ function plotPair(name, values, labels, measures, idx1, idx2, newFig)
 	hold on;
 	for col=1:3
 		val = values(labels == col, :);
+
+		% Plot scatters
 		plot(val(:, idx1), val(:, idx2), '.', 'color', colors(col, :), 'MarkerSize', markSize);
+
+		% Plot confidence ellipses
+		plotEllipse([val(:, idx1), val(:, idx2)], colors(col, :), 2);
 	end
+
+	% Plot overall confidence ellipse
+	plotEllipse([values(:, idx1), values(:, idx2)], 'k', 3);
 
 	title(name);
 	xlabel(measures(idx1));
@@ -32,4 +40,22 @@ function plotPair(name, values, labels, measures, idx1, idx2, newFig)
 		saveas(fig, strcat(pathstr, '.png'));
 		copyfile(strcat(pathstr, '.png'), strcat('img/pairs/', filename, '.png')); % Also save without timestamp
 	end
+end
+
+function plotEllipse(data, colr, linewidth, ciPercent)
+	if nargin < 4
+		ciPercent = 0.95; % 95% confidence interval
+	end
+
+	[eigvec, eigval] = eig(cov(data), 'vector');
+
+	% Create an ellipse with a size based on the eigenvalues
+	theta_grid = linspace(0, 2*pi);
+	ellipseMax = sqrt(max(eigval)) * cos(theta_grid);
+	ellipseMin = sqrt(min(eigval)) * sin(theta_grid);
+
+	% Scale and rotate the ellipse, then shift by the mean
+	ellipse = sqrt(chi2inv(ciPercent, 2)) * (eigvec * [ellipseMin; ellipseMax])' + mean(data);
+
+	plot(ellipse(:, 1), ellipse(:, 2), '-', 'color', colr,'LineWidth', linewidth)
 end
