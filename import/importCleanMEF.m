574b0ca707c344920bb857f1c13c26c3694dd049
James Bell <james@bellstone.ca>
2019-05-24 13:17:37 -0600

    Move flattenStructs into importCleanMEF

--- import/importCleanMEF.m
+++ import/importCleanMEF.m
@@ -9,3 +9,20 @@ function [values, participants, num, measures] = importCleanMEF(filepath, expect
 
 	[values, participants, num] = cleanData(values, participants, nanMethod);
 end
+
+function [flatVals, flatParts] = flattenStructs(structVals, structParts)
+	flatVals = [];
+	flatParts = [];
+	fields = fieldnames(structVals);
+	for i = 1:numel(fields)
+		thisParts = structParts.(fields{i});
+		thisVals = structVals.(fields{i});
+		for j = 1:length(thisParts)
+			% Only add the data if the participant hasn't already been added
+			if length(flatParts) < 2 || sum(ismember(flatParts, thisParts(j))) == 0
+				flatVals = [flatVals; thisVals(j, :)];
+				flatParts = [flatParts; thisParts(j)];
+			end
+		end
+	end
+end
