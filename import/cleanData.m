c3b415fb2e2a7e693630079807ddb40b9310728a
James Bell <james@bellstone.ca>
2019-05-24 13:17:27 -0600

    Remove unnecessary files

--- import/cleanData.m
+++ import/cleanData.m
@@ -8,3 +8,22 @@ function [values, participants, num] = cleanData(values, participants, nanMethod
 	end
 	canNum = size(values, 1);
 end
+
+function [vals, parts] = deleteNoSex(vals, parts)
+	hasSex = ismember(vals(:, 15), [2.0 1.0]);
+	if sum(~hasSex) > 0
+		fprintf('Deleting sexless %s.\n', parts(~hasSex));
+	end
+	vals = vals(hasSex, :);
+	parts = parts(hasSex);
+end
+
+function [dedupVals, dedupParts] = deduplicate(vals, parts)
+	orig = length(parts);
+	[~, indices] = unique(parts, 'first');
+	dedupParts = parts(sort(indices));
+	dedupVals = vals(sort(indices), :);
+	if length(dedupParts) ~= orig
+		fprintf('Deleted %d duplicates\n', orig-length(dedupParts));
+	end
+end
--- import/deduplicate.m
+++
@@ -1,9 +0,0 @@
-function [dedupVals, dedupParts] = deduplicate(vals, parts)
-	orig = length(parts);
-	[~, indices] = unique(parts, 'first');
-	dedupParts = parts(sort(indices));
-	dedupVals = vals(sort(indices), :);
-	if length(dedupParts) ~= orig
-		fprintf('Deleted %d duplicates\n', orig-length(dedupParts));
-	end
-end
