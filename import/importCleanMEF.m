9ee02ba613d54fe8bd0bca81cec875040aa72ec0
James Bell <james@bellstone.ca>
2019-05-24 13:26:28 -0600

    Fix bug (typo in extension name)

--- import/importCleanMEF.m
+++ import/importCleanMEF.m
@@ -1,6 +1,6 @@
 function [values, participants, num, measures] = importCleanMEF(filepath, expectedNames, nanMethod)
 	[~,~,ext] = fileparts(filepath);
-	if strcmp(ext, 'xlsx')
+	if strcmp(ext, '.xlsx')
 		[values, participants, measures] = mefimport(filepath, false, false, expectedNames);
 	else
 		[values, participants, measures] = importAllXLSX('none', filepath, expectedNames);
