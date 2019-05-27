1fb188a52e36208e54dbbde8cba33bdb3619b370
James Bell <james@bellstone.ca>
2019-05-27 09:49:51 -0600

    Remove unnecessary argument

--- saveNormative.m
+++ saveNormative.m
@@ -1,12 +1,9 @@
 % saveNormative loads multiple datasets and confirms they can be concatenated.
-function saveNormative(savefilepath, pathPrefix, nanMethod)
-	if nargin < 3
+function saveNormative(savefilepath, nanMethod)
+	if nargin < 2
 		nanMethod = 'IterateRegr';
-		if nargin < 2
-			pathPrefix = 'data/';
-			if nargin < 1
-				savefilepath = 'bin/batch-normative.mat';
-			end
+		if nargin < 1
+			savefilepath = 'bin/batch-normative.mat';
 		end
 	end
 
@@ -15,10 +12,10 @@ function saveNormative(savefilepath, pathPrefix, nanMethod)
 	measures = canonicalNamesNoTE20();
 
 	% Load the data
-	[canValues, canParticipants, canNum, measures] = importCleanMEF(strcat(pathPrefix, 'human/FESmedianAPB_Concatenated.xlsx'), measures, nanMethod);
-	[legValues, legParticipants, legNum] = importCleanMEF(strcat(pathPrefix, 'human/CPrepeatedmeasures.xlsx'), measures, nanMethod); % I'm not using the full set because QTRAC chokes on Excel import after around 130 rows
-	[japValues, japParticipants, japNum] = importCleanMEF(strcat(pathPrefix, 'Japan'), measures, nanMethod);
-	[porValues, porParticipants, porNum] = importCleanMEF(strcat(pathPrefix, 'Portugal'), measures, nanMethod);
+	[canValues, canParticipants, canNum, measures] = importCleanMEF('data/human/FESmedianAPB_Concatenated.xlsx', measures, nanMethod);
+	[legValues, legParticipants, legNum] = importCleanMEF('data/human/CPrepeatedmeasures.xlsx', measures, nanMethod); % I'm not using the full set because QTRAC chokes on Excel import after around 130 rows
+	[japValues, japParticipants, japNum] = importCleanMEF('data/Japan', measures, nanMethod);
+	[porValues, porParticipants, porNum] = importCleanMEF('data/Portugal', measures, nanMethod);
 
 	save(savefilepath, 'canValues', 'canParticipants', 'canNum', 'japValues', 'japParticipants', 'japNum', 'porValues', 'porParticipants', 'porNum', 'legValues', 'legParticipants', 'legNum', 'measures', 'nanMethod')
 end
