490cf6e5ac5e647095f2b1b8764015f6a604cd01
James Bell <james@bellstone.ca>
2019-06-06 12:04:31 -0600

    Export RC

--- import/convertXLSX.m
+++ import/convertXLSX.m
@@ -1,6 +1,7 @@
 % convertXLSX imports the Excel file at the given path and outputs it as MEM files
 function convertXLSX(filepath)
 	[data, participants, measureNames, ~, age, sex, temperature] = mefimport(filepath);
+	[rcDelay, rcVal] = mefRCimport(filepath, participants);
 
 	[dirpath, fileName] = fileparts(filepath);
 	[~,~] = mkdir(strcat(dirpath, "/convMEM")); % Read and ignore returns to suppress warning if dir exists.
@@ -9,6 +10,7 @@ function convertXLSX(filepath)
 		fileID = fopen(strcat(dirpath, "/convMEM/", participants(i), ".MEM"),'w');
 
 		writeHeader(fileID, filepath, participants(i), age(i), sex(i), temperature(i));
+		writeRC(fileID, rcDelay, rcVal);
 
 		fclose(fileID);
 	end
@@ -33,5 +35,15 @@ function writeHeader(fileID, filepath, name, age, sex, temperature)
 	fprintf(fileID, " NC/disease:        	\n");
 	fprintf(fileID, " Operator:          	\n");
 	fprintf(fileID, " Comments:          	this MEM file was created from an Excel file\n");
+	fprintf(fileID, "\n");
+end
+
+function writeRC(fileID, rcDelay, rcVal)
+	fprintf(fileID, "   RECOVERY CYCLE DATA\n\n");
+	fprintf(fileID, "                     	Interval (ms)       	  Threshold change (%%)\n");
+
+	for i=1:length(rcDelay)
+		fprintf(fileID, "RC1.%d               	 %f                	%f\n", i, rcDelay(i), rcVal(i));
+	end
 	fprintf(fileID, "\n");
 end
