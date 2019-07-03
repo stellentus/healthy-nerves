efdc36a77c007ff6292d4f04821feb4ebdd642b1
James Bell <james@stellentus.com>
2019-07-03 15:52:59 -0600

    Print r^2 in regression

--- normativeMultiRegress.m
+++ normativeMultiRegress.m
@@ -21,8 +21,8 @@ function normativeMultiRegress(values)
 	astCols = values(:,[sexIndex, ageIndex, tempIndex]);
 
 	if nargout == 0
-		fprintf(" Measure Name        & Sex Coeff (p)      & Age Coeff (p)      & Temp Coeff (p)    \n");
-		fprintf("-------------------- & ------------------ & ------------------ & ------------------\n");
+		fprintf(" Measure Name        & Sex Coeff (p,r)        & Age Coeff (p,r)        & Temp Coeff (p,r)      \n");
+		fprintf("-------------------- & ---------------------- & ---------------------- & ----------------------\n");
 	end
 
 	measures = altNames();
@@ -49,14 +49,15 @@ function normativeMultiRegress(values)
 	fprintf("\t%s\n", insigMeasures);
 end
 
-function [str] = dispCoeff(coeff, pval, inmodel, ind)
+function [str] = dispCoeff(coeff, pval, inmodel, rsq, ind)
 	if ~inmodel(ind)
-		str = "       ---        ";
+		str = "         ---          ";
 		return;
 	end
 
 	coeff = coeff(ind);
 	pval = pval(ind);
+	rsq = rsq(ind);
 
 	coPre = " ";
 	if coeff < 0
@@ -71,18 +72,19 @@ function [str] = dispCoeff(coeff, pval, inmodel, ind)
 	end
 
 	if pval < 0.0001
-		pStr = '(<0.0001)';
+		pStr = '<0.0001';
 	elseif pval < 0.001
-		pStr = '(<0.001)';
+		pStr = '<0.001';
 	elseif pval < 0.01
-		pStr = sprintf('(%.3f)', pval);
+		pStr = sprintf('%.3f', pval);
 	elseif pval < 0.1
-		pStr = sprintf('(%.3f)', pval);
+		pStr = sprintf('%.3f', pval);
 	else
-		pStr = sprintf('(%.2f)', pval);
+		pStr = sprintf('%.2f', pval);
 	end
 
-	str = sprintf(coeffStr + " %9s", coeff, pStr);
+	parens = sprintf("(%s, %.0f)", pStr, rsq*100);
+	str = sprintf(coeffStr + " %13s", coeff, parens);
 end
 
 function [str] = altNames()
@@ -94,16 +96,29 @@ function [ids] = altInds()
 end
 
 function [str] = stepWiseString(thisMeas, astCols, thisCol)
-	[b, ~, modelp, inmodel] = stepwisefit(astCols, thisCol, 'penter', 0.002, 'premove', 0.01, 'display', 'off');
+	[b, ~, modelp, inmodel, ~, ~, history] = stepwisefit(astCols, thisCol, 'penter', 0.002, 'premove', 0.01, 'display', 'off');
 
 	if ~inmodel(1) && ~inmodel(2) && ~inmodel(3)
 		str = "";
 		return
 	end
 
-	strAge = dispCoeff(b, modelp, inmodel, 1);
-	strSex = dispCoeff(b, modelp, inmodel, 2);
-	strTemp = dispCoeff(b, modelp, inmodel, 3);
+	% Calculate r^2.
+	n = length(thisCol);
+	adjrsq = 1 - history.rmse.^2/var(thisCol);
+	adjrsq = adjrsq - [0, adjrsq(1:end-1)]; % We want each value as the marginal increase past the previous variable addition.
+	clear rsq n;
 
-	str = sprintf("%20s & %18s & %18s & %18s", thisMeas, strAge, strSex, strTemp);
+	% Figure out which index changed for each r^2. This code only works if no index is ever removed.
+	shiftHistory = history.in - [0 0 0; history.in(1:end-1,:)]; % history.in contains all indices at each point, but we just want the diff
+	[iR,iC]=find(shiftHistory); % Based on this diff, get the list of indices as they were added
+	sr = sortrows([iR,iC]);     % But now we need to sort it.
+	rsq(sr(:,2)) = adjrsq;      % Finally, access based on these values.
+	clear shiftHistory iR iC sr;
+
+	strAge = dispCoeff(b, modelp, inmodel, rsq, 1);
+	strSex = dispCoeff(b, modelp, inmodel, rsq, 2);
+	strTemp = dispCoeff(b, modelp, inmodel, rsq, 3);
+
+	str = sprintf("%20s & %22s & %22s & %22s", thisMeas, strAge, strSex, strTemp);
 end
