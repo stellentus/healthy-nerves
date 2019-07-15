3c1cd20c24664af60277ad732702e27c38b128b2
James Bell <james@stellentus.com>
2019-07-15 11:39:25 -0600

    Load all of the correct data for neuroinformatics

--- neuroinformatics.m
+++ neuroinformatics.m
@@ -4,21 +4,22 @@ function neuroinformatics(newPath)
 	end
 	mkdir(newPath);
 
-	saveNormative();
-	saveNonNormative();
+	saveNormative('bin/batch-normative.mat', 'IterateCascadeAuto');
+	saveNormative('bin/missing-normative.mat', 'none');
+	saveNonNormative('bin/non-normative.mat', 'IterateCascadeAuto');
 
 	addpath batches;
 	printStats('bin/batch-normative.mat');
 	rmpath batches;
 
-	missmiss('standard', 'parallelize', true, 'iters', 30, 'fixedSeed', false, 'numToUse', 0);
+	missmiss('standard', 'parallelize', true, 'iters', 30, 'fixedSeed', false, 'numToUse', 0, 'file', "bin/missing-normative.mat");
 	copyfile('img/missmiss/244-standard-value.png', strcat(newPath, 'missmiss-244-standard-value.png'));
 	copyfile('img/missmiss/244-standard-times.png', strcat(newPath, 'missmiss-244-standard-times.png'));
 
-	missmiss('standard', 'parallelize', true, 'iters', 30, 'fixedSeed', false, 'numToUse', 40);
+	missmiss('standard', 'parallelize', true, 'iters', 30, 'fixedSeed', false, 'numToUse', 40, 'file', "bin/missing-normative.mat");
 	copyfile('img/missmiss/40-standard-value.png', strcat(newPath, 'missmiss-40-standard-value.png'));
 
-	batcherFigures();
+	batcherFigures('normfile', "bin/batch-normative.mat", 'nonnormfile', "bin/non-normative.mat");
 	copyfile('img/batch/norm-rand.png', strcat(newPath, 'batch-norm-rand.png'));
 	copyfile('img/batch/country-splits.png', strcat(newPath, 'batch-country-splits.png'));
 	copyfile('img/batch/vs-nonnorm.png', strcat(newPath, 'batch-vs-nonnorm.png'));
